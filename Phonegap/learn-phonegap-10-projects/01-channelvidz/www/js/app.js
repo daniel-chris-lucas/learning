4aeb88d89277ee2a2c0876cbf03f0f427b37026b
Daniel Lucas <daniel.chris.lucas@gmail.com>
2015-10-25 14:02:05 +0100

    ChannelVidz: Added connection to youtube API

--- Phonegap/learn-phonegap-10-projects/01-channelvidz/www/js/app.js
+++ Phonegap/learn-phonegap-10-projects/01-channelvidz/www/js/app.js
@@ -3,5 +3,22 @@ $(document).ready(function () {
 });
 
 function onDeviceReady () {
+    var channel = 'TechGuyWeb';
 
+    getPlaylist(channel);
+}
+
+function getPlaylist (channel) {
+    $('#vidlist').html('');
+    $.get(
+        "https://www.googleapis.com/youtube/v3/channels",
+        {
+            part: 'contentDetails',
+            forUsername: channel,
+            key: 'AIzaSyBo7yWza9H0Mj7xcpumRK_68HA_7nrTc20'
+        },
+        function (data) {
+            console.log(data);
+        }
+    );
 }
