8bd1892538a11f297ce1970161e82073873ca524
Daniel Lucas <daniel.chris.lucas@gmail.com>
2015-10-25 14:14:32 +0100

    ChannelVidz: Loaded video list from a channel on YouTube

--- Phonegap/learn-phonegap-10-projects/01-channelvidz/www/js/app.js
+++ Phonegap/learn-phonegap-10-projects/01-channelvidz/www/js/app.js
@@ -18,7 +18,34 @@ function getPlaylist (channel) {
             key: 'AIzaSyBo7yWza9H0Mj7xcpumRK_68HA_7nrTc20'
         },
         function (data) {
-            console.log(data);
+            $.each(data.items, function (i, item) {
+                console.log(item);
+                var playlistId = item.contentDetails.relatedPlaylists.uploads;
+                getVideos(playlistId, 10);
+            });
         }
     );
+}
+
+function getVideos (playlistId, maxResults) {
+    $.get(
+        "https://www.googleapis.com/youtube/v3/playlistItems",
+        {
+            part: 'snippet',
+            maxResults: maxResults,
+            playlistId: playlistId,
+            key: 'AIzaSyBo7yWza9H0Mj7xcpumRK_68HA_7nrTc20'
+        },
+        function (data) {
+            var output;
+            $.each(data.items, function (i, item) {
+                var id    = item.snippet.resourceId.videoId,
+                    title = item.snippet.title,
+                    thumb = item.snippet.thumbnails.default.url;
+
+                $('#vidlist').append('<li videoId="'+id+'"><img src="'+thumb+'"><h3>'+title+'</h3></li>');
+                $('#vidlist').listview('refresh');
+            })
+        }
+    )
 }
