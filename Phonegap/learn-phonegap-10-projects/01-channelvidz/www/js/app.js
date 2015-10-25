1e420a25465dbe6966df3cc6042f0d74fb91960f
Daniel Lucas <daniel.chris.lucas@gmail.com>
2015-10-25 15:42:19 +0100

    Finished ChannelVidz

--- Phonegap/learn-phonegap-10-projects/01-channelvidz/www/js/app.js
+++ Phonegap/learn-phonegap-10-projects/01-channelvidz/www/js/app.js
@@ -3,9 +3,41 @@ $(document).ready(function () {
 });
 
 function onDeviceReady () {
-    var channel = 'TechGuyWeb';
+    // Check LocalStorage for channel
+    if (localStorage.channel == null || localStorage.channel == '') {
+        // Ask user for channel
+        $('#popupDialog').popup("open");
+    } else {
+        var channel = localStorage.getItem('channel');
+    }
 
     getPlaylist(channel);
+
+    $(document).on('click', '#vidlist li', function () {
+        showVideo($(this).attr('videoId'));
+    });
+
+    $('#channelBtnOK').click(function () {
+        var channel = $('#channelName').val();
+        setChannel(channel);
+        getPlaylist(channel);
+    });
+
+    $('#saveOptions').click(function () {
+        saveOptions();
+    });
+
+    $('#clearChannel').click(function () {
+        clearChannel();
+    });
+
+    $(document).on('pageinit', '#options', function (e) {
+        var channel    = localStorage.getItem('channel'),
+            maxResults = localStorage.getItem('maxResults');
+
+        $('#channelNameOptions').attr('value', channel);
+        $('#maxResultsOptions').attr('value', maxResults);
+    })
 }
 
 function getPlaylist (channel) {
@@ -21,7 +53,7 @@ function getPlaylist (channel) {
             $.each(data.items, function (i, item) {
                 console.log(item);
                 var playlistId = item.contentDetails.relatedPlaylists.uploads;
-                getVideos(playlistId, 10);
+                getVideos(playlistId, localStorage.getItem('maxResults'));
             });
         }
     );
@@ -48,4 +80,40 @@ function getVideos (playlistId, maxResults) {
             })
         }
     )
+}
+
+function showVideo (id) {
+    $('#logo').hide();
+    var output = '<iframe width="100%" height="250" src="https://www.youtube.com/embed/'+id+'" frameborder="0" allowfullscreen></iframe>';
+    $('#showVideo').html(output);
+}
+
+function setChannel (channel) {
+    localStorage.setItem('channel', channel);
+}
+
+function setMaxResults (maxResults) {
+    localStorage.setItem('maxResults', maxResults);
+}
+
+function saveOptions () {
+    var channel = $('#channelNameOptions').val();
+    setChannel(channel);
+
+    var maxResults = $('#maxResultsOptions').val();
+    setMaxResults(maxResults);
+
+    $('body').pagecontainer('change', '#main', {options});
+    getPlaylist(channel);
+}
+
+function clearChannel () {
+    localStorage.removeItem('channel');
+    $('body').pagecontainer('change', '#main', {options});
+
+    // Clear list
+    $('#vidlist').html('');
+
+    // Show popup
+    $('#popupDialog').popup('open');
 }
