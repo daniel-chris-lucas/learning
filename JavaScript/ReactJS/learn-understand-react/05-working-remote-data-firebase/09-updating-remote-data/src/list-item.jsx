237ca8da26f9caedaf3e80cade431b26c8deeeff
Daniel Lucas <daniel.chris.lucas@gmail.com>
2015-12-27 19:18:45 +0100

    Saving done state to firebase

---
+++ JavaScript/ReactJS/learn-understand-react/05-working-remote-data-firebase/09-updating-remote-data/src/list-item.jsx
@@ -0,0 +1,42 @@
+var React = require('react');
+var Firebase = require('firebase');
+var rootUrl = 'https://blistering-torch-3630.firebaseio.com/';
+
+module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      text: this.props.item.text,
+      done: this.props.item.done
+    };
+  },
+
+  componentWillMount: function() {
+    this.fb = new Firebase(rootUrl + 'items/' + this.props.item.id);
+  },
+
+  render: function() {
+    return <div className="input-group">
+      <span className="input-group-addon">
+        <input
+          type="checkbox"
+          checked={this.state.done}
+          onChange={this.handleDoneChange} />
+      </span>
+      <input type="text"
+        className="form-control"
+        value={this.state.text} />
+      <span className="input-group-btn">
+        <button className="btn btn-default">
+          Delete
+        </button>
+      </span>
+    </div>
+  },
+
+  handleDoneChange: function(event) {
+    var update = {done: event.target.checked};
+
+    this.setState(update);
+    this.fb.update(update);
+  }
+});
---
+++ JavaScript/ReactJS/learn-understand-react/05-working-remote-data-firebase/09-updating-remote-data/src/list.jsx
@@ -0,0 +1,37 @@
+var React = require('react');
+var ListItem = require('./list-item');
+
+module.exports = React.createClass({
+  render: function() {
+    console.log(this.props);
+
+    return <div>
+      {this.renderList()}
+    </div>
+  },
+
+  renderList: function() {
+    if(this.props.items && Object.keys(this.props.items).length === 0) {
+      return <h4>
+        Add a todo to get started.
+      </h4>
+    } else {
+      var children = [];
+
+      for(var key in this.props.items) {
+        var item = this.props.items[key];
+        item.key = key;
+
+        children.push(
+          <ListItem
+            item={item}
+            key={key}
+          >
+          </ListItem>
+        );
+      }
+
+      return children;
+    }
+  }
+});
