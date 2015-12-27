ca383a82b1eed94fbcaf05a839104864b1416dc8
Daniel Lucas <daniel.chris.lucas@gmail.com>
2015-12-27 23:06:08 +0100

    Refactoring to move routes to specialized routing file

---
+++ JavaScript/ReactJS/learn-understand-react/06-flux-react-router/02-refactor-separate-rendering-routing/src/app.jsx
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Routes = require('./routes');
+
+// var element = React.createElement(Hello, {});
+ReactDOM.render(Routes, document.querySelector('.container'));
