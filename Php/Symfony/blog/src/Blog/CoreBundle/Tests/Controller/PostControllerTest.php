4ed62f15850c1d0f8ad1eb5f82092f2df10c1328
Daniel Lucas <daniel.chris.lucas@gmail.com>
2016-01-09 00:34:31 +0100

    Added test to check loading of blog index

--- Php/Symfony/blog/src/Blog/CoreBundle/Tests/Controller/PostControllerTest.php
+++ Php/Symfony/blog/src/Blog/CoreBundle/Tests/Controller/PostControllerTest.php
@@ -4,13 +4,22 @@ namespace Blog\CoreBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * Class PostControllerTest
+ * @package Blog\CoreBundle\Tests\Controller
+ */
 class PostControllerTest extends WebTestCase
 {
+    /**
+     * Test posts index
+     */
     public function testIndex()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($client->getResponse()->isSuccessful(), 'The response was not successful');
     }
 
 }
