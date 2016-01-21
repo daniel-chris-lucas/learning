d4abf5133a94af3ba020a6d61e10bbe1978e35ed
Daniel Lucas <daniel.chris.lucas@gmail.com>
2016-01-21 23:59:10 +0100

    Added Product class

---
+++ JavaScript/Angular2/03-inventory-app/app.js
@@ -0,0 +1,11 @@
+var Product = (function () {
+    function Product(sku, name, imageUrl, department, price) {
+        this.sku = sku;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.department = department;
+        this.price = price;
+    }
+    return Product;
+})();
+//# sourceMappingURL=app.js.map
---
+++ JavaScript/Angular2/03-inventory-app/app.ts
@@ -0,0 +1,11 @@
+class Product {
+  constructor(
+    public sku: string,
+    public name: string,
+    public imageUrl: string,
+    public department: string[],
+    public price: number
+  ) {
+    
+  }
+}
