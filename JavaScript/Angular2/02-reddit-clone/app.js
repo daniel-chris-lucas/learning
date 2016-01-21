655894b405ccd9431ea35fa30c327510f6cf0faf
Daniel Lucas <daniel.chris.lucas@gmail.com>
2016-01-21 21:40:38 +0100

    Added RedditArticle component

--- JavaScript/Angular2/02-reddit-clone/app.js
+++ JavaScript/Angular2/02-reddit-clone/app.js
@@ -9,7 +9,7 @@ System.register(["angular2/platform/browser", "angular2/core"], function(exports
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var browser_1, core_1;
-    var RedditApp;
+    var ArticleComponent, RedditApp;
     return {
         setters:[
             function (browser_1_1) {
@@ -19,6 +19,32 @@ System.register(["angular2/platform/browser", "angular2/core"], function(exports
                 core_1 = core_1_1;
             }],
         execute: function() {
+            ArticleComponent = (function () {
+                function ArticleComponent() {
+                    this.title = 'Angular 2';
+                    this.link = 'http://angular.io';
+                    this.votes = 10;
+                }
+                ArticleComponent.prototype.voteUp = function () {
+                    this.votes += 1;
+                    return false;
+                };
+                ArticleComponent.prototype.voteDown = function () {
+                    this.votes -= 1;
+                    return false;
+                };
+                ArticleComponent = __decorate([
+                    core_1.Component({
+                        selector: 'reddit-article',
+                        host: {
+                            class: 'row'
+                        },
+                        template: "\n    <div class=\"four wide column center aligned votes\">\n      <div class=\"ui statistic\">\n        <div class=\"value\">\n          {{ votes }}\n        </div>\n        <div class=\"label\">\n          Points\n        </div>\n      </div>\n    </div>\n    <div class=\"twelve wide column\">\n      <a class=\"ui large header\" href=\"{{ link }}\">\n        {{ title }}\n      </a>\n      <ul class=\"ui big horizontal list voters\">\n        <li class=\"item\">\n          <a href (click)=\"voteUp()\">\n            <i class=\"arrow up icon\"></i>\n            upvote\n          </a>\n        </li>\n        <li class=\"item\">\n          <a href (click)=\"voteDown()\">\n            <i class=\"arrow down icon\"></i>\n            downvote\n          </a>\n        </li>\n      </ul>\n    </div>\n  "
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], ArticleComponent);
+                return ArticleComponent;
+            })();
             RedditApp = (function () {
                 function RedditApp() {
                 }
@@ -28,7 +54,8 @@ System.register(["angular2/platform/browser", "angular2/core"], function(exports
                 RedditApp = __decorate([
                     core_1.Component({
                         selector: 'reddit',
-                        template: "\n    <form class=\"ui large form segment\">\n      <h3 class=\"ui header\">Add a Link</h3>\n\n      <div class=\"field\">\n        <label for=\"title\">Title:</label>\n        <input name=\"title\" #newtitle>\n      </div>\n      <div class=\"field\">\n        <label for=\"link\">Link:</label>\n        <input name=\"link\" #newlink>\n      </div>\n\n      <button (click)=\"addArticle(newtitle, newlink)\"\n              class=\"ui positive right floated button\">\n        Submit link\n      </button>\n    </form>\n  "
+                        directives: [ArticleComponent],
+                        template: "\n    <form class=\"ui large form segment\">\n      <h3 class=\"ui header\">Add a Link</h3>\n\n      <div class=\"field\">\n        <label for=\"title\">Title:</label>\n        <input name=\"title\" #newtitle>\n      </div>\n      <div class=\"field\">\n        <label for=\"link\">Link:</label>\n        <input name=\"link\" #newlink>\n      </div>\n\n      <button (click)=\"addArticle(newtitle, newlink)\"\n              class=\"ui positive right floated button\">\n        Submit link\n      </button>\n    </form>\n\n    <div class=\"ui grid posts\">\n      <reddit-article></reddit-article>\n    </div>\n  "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], RedditApp);
