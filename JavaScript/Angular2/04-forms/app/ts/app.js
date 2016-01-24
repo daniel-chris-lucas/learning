3637bbff0772210fd22a7c7e8e53ac7bc3cc73f7
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-01-25 00:10:26 +0100

    Started forms

---
+++ JavaScript/Angular2/04-forms/app/ts/app.js
@@ -0,0 +1,6 @@
+"use strict";
+require('../css/styles.css');
+require('../css/semantic.min.css');
+require('../images/ng-book-2-minibook.png');
+require('../images/favicon-32x32.png');
+require('../images/favicon.ico');
---
+++ JavaScript/Angular2/04-forms/app/ts/app.ts
@@ -0,0 +1,15 @@
+/*
+ * Angular
+ */
+import { Component } from 'angular2/core';
+import { bootstrap } from 'angular2/platform/browser';
+
+/*
+ * We're using Webpack to load our CSS which is why we use `require` instead of
+ * `import` here
+ */
+require('../css/styles.css');
+require('../css/semantic.min.css');
+require('../images/ng-book-2-minibook.png');
+require('../images/favicon-32x32.png');
+require('../images/favicon.ico');
