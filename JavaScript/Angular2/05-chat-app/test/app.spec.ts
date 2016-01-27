b44d054a192567d09d268899178e16fd29b1c0d8
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-01-27 21:32:05 +0100

    Started project. Created User, Thread and Message models

---
+++ JavaScript/Angular2/05-chat-app/test/app.spec.ts
@@ -0,0 +1,22 @@
+import {
+  it,
+  beforeEachProviders
+} from 'angular2/testing';
+
+// import {MessagesService} from '../app/ts/services/services';
+
+import {Calculator} from 'calculator';
+
+describe('Hello', () => {
+
+  it('should test', () => {
+    // let m = MessagesService;
+  });
+
+  it('should add', () => {
+    let c = new Calculator();
+    expect(true).toBe(true);
+    expect(c.add(1,2)).toBe(3);
+  });
+
+});
