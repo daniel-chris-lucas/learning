ee65f7ba03ce13f856b7b5e0d9e3648047b8135b
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-05-07 20:10:43 +0200

    Added vue and laravel workflow

---
+++ JavaScript/VueJS/learning-vue-1.0/13-vue-and-laravel-workflow/resources/assets/js/components/Alert.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="alertClasses" v-show="show">
+    <slot></slot>
+    <span class="Alert__close" @click="show = false">x</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['type'],
+
+    data() {
+      return {
+        show: true
+      };
+    },
+
+    computed: {
+      alertClasses() {
+        var type = this.type;
+
+        return {
+          'Alert': true,
+          'Alert--Success': type == 'success',
+          'Alert--Error': type == 'error'
+        };
+      }
+    }
+  };
+</script>
