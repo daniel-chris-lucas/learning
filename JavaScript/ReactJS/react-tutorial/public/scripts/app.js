ad7c981cf06ac2ec33f5c629438e79460c655818
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-08-18 00:13:49 +0200

    Added markdown to comments

--- JavaScript/ReactJS/react-tutorial/public/scripts/app.js
+++ JavaScript/ReactJS/react-tutorial/public/scripts/app.js
@@ -1,11 +1,17 @@
 var Comment = React.createClass({
+    rawMarkup: function () {
+        var md = new Remarkable();
+        var rawMarkup = md.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    },
+
     render: function () {
         return (
             <div className="comment">
                 <h2 className="commentAuthor">
                     {this.props.author}
                 </h2>
-                {this.props.children}
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
             </div>
         );
     }
