eeef66d4bceb55f4bea8c235bcc94eaf6d7ae291
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-08-21 18:28:58 +0200

    Added more containers vs presentational components

---
+++ JavaScript/ReactJS/reactjsprogram/reactjsfundamentals/08_more_container_vs_presentational_components/README.md
@@ -0,0 +1,5 @@
+## More Containers vs Presentation Components
+
+Create Results and ResultsContainer components.
+
+Use github API to calculate score of each player.
---
+++ JavaScript/ReactJS/reactjsprogram/reactjsfundamentals/08_more_container_vs_presentational_components/app/components/ConfirmBattle.js
@@ -0,0 +1,48 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+var styles = require('../styles');
+var Link = require('react-router').Link;
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
+
+function puke (object) {
+    return <pre>{JSON.stringify(object, null, ' ')}</pre>
+}
+
+function ConfirmBattle (props) {
+    return props.isLoading === true
+        ? <p>LOADING!</p>
+        : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+            <h1>Confirm Players</h1>
+            <div className="col-sm-8 col-sm-offset-2">
+                <UserDetailsWrapper header="Player One">
+                    <UserDetails info={props.playersInfo[0]} />
+                </UserDetailsWrapper>
+                <UserDetailsWrapper header="Player Two">
+                    <UserDetails info={props.playersInfo[1]} />
+                </UserDetailsWrapper>
+            </div>
+            <div className="col-sm-8 col-sm-offset-2">
+                <div className="col-sm-12" style={styles.space}>
+                    <button type="button" className="btn btn-lg btn-success" onClick={props.onInitiateBattle}>
+                        Initiate Battle
+                    </button>
+                </div>
+                <div className="col-sm-12" style={styles.space}>
+                    <Link to="/playerOne">
+                        <button type="button" className="btn btn-lg btn-danger">
+                            Reselect Players
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+}
+
+ConfirmBattle.propTypes = {
+    isLoading: PropTypes.bool.isRequired,
+    onInitiateBattle: PropTypes.func.isRequired,
+    playersInfo: PropTypes.array.isRequired
+};
+
+module.exports = ConfirmBattle;
