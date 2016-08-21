467c29b6c703ff2460a81d1da6d3b4b0ae55afc4
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-08-21 19:45:55 +0200

    Added highly reusable components

---
+++ JavaScript/ReactJS/reactjsprogram/reactjsfundamentals/10_highly_reusable_components/README.md
@@ -0,0 +1,5 @@
+## Highly Reusable Components
+
+Add Loading component with text and speed props.
+
+Defaults to text: 'Loading' & speed: 300
---
+++ JavaScript/ReactJS/reactjsprogram/reactjsfundamentals/10_highly_reusable_components/app/components/Results.js
@@ -0,0 +1,63 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+var styles = require('../styles');
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
+var Link = require('react-router').Link;
+var MainContainer = require('./MainContainer');
+var Loading = require('./Loading');
+
+function StartOver () {
+    return (
+        <div className="col-sm-12" style={styles.space}>
+            <Link to="playerOne">
+                <button type="button" className="btn btn-lg btn-danger">
+                    Start Over
+                </button>
+            </Link>
+        </div>
+    );
+}
+
+function Results (props) {
+    if (props.isLoading === true) {
+        return (
+            <Loading text="One Moment" speed={100} />
+        );
+    }
+
+    if (props.scores[0] === props.scores[1]) {
+        return (
+            <MainContainer>
+                <h1>It's a tie!</h1>
+                <StartOver />
+            </MainContainer>
+        );
+    }
+
+    var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
+    var losingIndex = winningIndex === 0 ? 1 : 0;
+
+    return (
+        <MainContainer>
+            <h1>Results</h1>
+            <div className="col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
+                <UserDetailsWrapper header="Winner">
+                    <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]} />
+                </UserDetailsWrapper>
+                <UserDetailsWrapper header="Loser">
+                    <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]} />
+                </UserDetailsWrapper>
+            </div>
+            <StartOver />
+        </MainContainer>
+    );
+}
+
+Results.propTypes = {
+    isLoading: PropTypes.bool.isRequired,
+    playersInfo: PropTypes.array.isRequired,
+    scores: PropTypes.array.isRequired
+};
+
+module.exports = Results;
