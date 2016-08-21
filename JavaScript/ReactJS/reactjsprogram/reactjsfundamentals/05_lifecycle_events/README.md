fa0595b1fcddcdfa0bed2879f18f0080817dca6e
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-08-21 14:06:44 +0200

    Added lifecycle events

---
+++ JavaScript/ReactJS/reactjsprogram/reactjsfundamentals/05_lifecycle_events/README.md
@@ -0,0 +1,5 @@
+## React Lifecycle Events
+
+Create ConfirmBattleContainer and ConfirmBattle components.
+
+Quick intro to getInitialState and componentDidMount lifecycle events.
---
+++ JavaScript/ReactJS/reactjsprogram/reactjsfundamentals/05_lifecycle_events/app/index.js
@@ -0,0 +1,26 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
+var Raven = require('raven-js');
+
+var sentryKey = '599e9c1fb5044e73b18919b08d3ccca8';
+var sentryApp = '93245';
+var sentryURL = 'https://' + sentryKey + '@app.getsentry.com/' + sentryApp;
+
+var _APP_INFO = {
+    name: 'Github Battle',
+    branch: 'children_and_router',
+    version: '1.0'
+}
+
+Raven.config(sentryURL, {
+    release: _APP_INFO.version,
+    tags: {
+        branch: _APP_INFO.branch
+    }
+}).install();
+
+ReactDOM.render(
+    routes,
+    document.getElementById('app')
+);
