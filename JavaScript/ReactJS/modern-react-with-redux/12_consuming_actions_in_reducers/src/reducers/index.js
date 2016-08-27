ecde9da973baf08541a889c61f9cb4183b765ab8
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-08-27 23:20:27 +0200

    Added BookDetail container and bound to redux

--- JavaScript/ReactJS/modern-react-with-redux/12_consuming_actions_in_reducers/src/reducers/index.js
+++ JavaScript/ReactJS/modern-react-with-redux/12_consuming_actions_in_reducers/src/reducers/index.js
@@ -4,7 +4,7 @@ import ActiveBookReducer from './reducer_active_book'
 
 const rootReducer = combineReducers({
   books: BooksReducer,
-  activeBook: ActiveBook
+  activeBook: ActiveBookReducer
 });
 
 export default rootReducer;
