e0b290f84ed703c2fe6a85a3a8cdd0bd26bc4642
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-08-27 14:56:56 +0200

    User can click on video to load

--- JavaScript/ReactJS/modern-react-with-redux/07_youtube_search/src/index.js
+++ JavaScript/ReactJS/modern-react-with-redux/07_youtube_search/src/index.js
@@ -11,10 +11,16 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
@@ -22,8 +28,10 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]} />
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
             </div>
         );
     }
