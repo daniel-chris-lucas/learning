a93c83402cb8001bf7f39afc1b07cc3d59ae27fc
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-08-27 14:10:10 +0200

    Added VideoList and VideoListItem components

---
+++ JavaScript/ReactJS/modern-react-with-redux/07_youtube_search/src/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
+const API_KEY = 'AIzaSyB-RLb_qA2w9XwDjbPBQrBVKc3RXbNdXjI';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
