40d4b17d749756c692c0c6ef1932857b4f3f2b32
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-08-28 17:09:21 +0200

    Added city names to WeatherList component

---
+++ JavaScript/ReactJS/modern-react-with-redux/21_mapping_props_to_a_render_helper/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'cc8c06050986c92e450204a5dc60e99f';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
