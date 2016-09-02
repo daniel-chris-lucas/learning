221afe3bfaf255cd87f6503c7d8ab6535b3731f1
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-03 00:25:29 +0200

    Set up route and component for new posts

---
+++ JavaScript/ReactJS/modern-react-with-redux/28_creating_new_posts/src/components/posts_index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
+
+class PostsIndex extends Component {
+    componentWillMount() {
+        this.props.fetchPosts();
+    }
+
+    render() {
+        return (
+            <div>List of blog posts</div>
+        );
+    }
+}
+
+export default connect(null, { fetchPosts })(PostsIndex);
