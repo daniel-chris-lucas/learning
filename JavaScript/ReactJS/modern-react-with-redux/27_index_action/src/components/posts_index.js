500b1e508181b24a7b58f0d3994598c237a62cae
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-03 00:17:46 +0200

    Bound fetchPosts action to PostIndex mount event

--- JavaScript/ReactJS/modern-react-with-redux/27_index_action/src/components/posts_index.js
+++ JavaScript/ReactJS/modern-react-with-redux/27_index_action/src/components/posts_index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
     componentWillMount() {
-        
+        this.props.fetchPosts();
     }
 
     render() {
@@ -12,4 +15,4 @@ class PostsIndex extends Component {
     }
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
