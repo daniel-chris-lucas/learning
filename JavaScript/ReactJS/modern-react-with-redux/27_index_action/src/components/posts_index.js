9984c1ab087b57e4980f86f524b05e5a3b9647e4
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-03 00:10:06 +0200

    Converted PostsIndex to a component

--- JavaScript/ReactJS/modern-react-with-redux/27_index_action/src/components/posts_index.js
+++ JavaScript/ReactJS/modern-react-with-redux/27_index_action/src/components/posts_index.js
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-    return <div>List of blog posts.</div>;
+class PostsIndex extends Component {
+    componentWillMount() {
+        
+    }
+
+    render() {
+        return (
+            <div>List of blog posts</div>
+        );
+    }
 }
+
+export default PostsIndex;
