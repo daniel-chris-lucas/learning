10ab4c76d8791c83304c064e0d0784a0b2748f0a
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-03 00:18:20 +0200

    Deleted unneeded import

--- JavaScript/ReactJS/modern-react-with-redux/27_index_action/src/components/posts_index.js
+++ JavaScript/ReactJS/modern-react-with-redux/27_index_action/src/components/posts_index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
