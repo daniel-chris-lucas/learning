0665fad1c2a4f2f801d5379e63dd293ee10ab625
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-03 00:58:00 +0200

    Added form component and redux-form package

---
+++ JavaScript/ReactJS/modern-react-with-redux/30_forms_and_form_submission/src/components/posts_new.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+    render() {
+        return (
+            <form>
+                <h3>Create a New Post</h3>
+                <div className="form-group">
+                    <label>Title</label>
+                    <input type="text" className="form-control" />
+                </div>
+
+                <div className="form-group">
+                    <label>Categories</label>
+                    <input type="text" className="form-control" />
+                </div>
+
+                <div className="form-group">
+                    <label>Content</label>
+                    <textarea className="form-control" />
+                </div>
+
+                <button type="submit" className="btn btn-primary">
+                    Submit
+                </button>
+            </form>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'PostsNewForm',
+    fields: ['title', 'categories', 'content']
+})(PostsNew);
