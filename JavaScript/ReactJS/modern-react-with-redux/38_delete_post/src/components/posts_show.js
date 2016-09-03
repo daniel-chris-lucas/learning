932540b2984e96c460aa408df546075b032d6173
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-03 17:05:49 +0200

    Can delete post

---
+++ JavaScript/ReactJS/modern-react-with-redux/38_delete_post/src/components/posts_show.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
+
+class PostsShow extends Component {
+    componentWillMount() {
+        this.props.fetchPost(this.props.params.id);
+    }
+
+    onDeleteClick() {
+        this.props.deletePost(this.props.params.id);
+    }
+
+    render() {
+        const { post } = this.props;
+
+        if (!post) {
+            return <div>Loading...</div>;
+        }
+
+        return (
+            <div>
+                <Link to="/">Back to Index</Link>
+
+                <button className="btn btn-danger pull-xs-right"
+                        onClick={this.onDeleteClick.bind(this)}
+                >
+                     Delete Post
+                </button>
+
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return { post: state.posts.post };
+}
+
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
