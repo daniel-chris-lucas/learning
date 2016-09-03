8e192c2a1f03a904dfec13ef6137a61b1eefb788
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-03 14:43:13 +0200

    Added binding for redux-form to form fields

---
+++ JavaScript/ReactJS/modern-react-with-redux/31_passing_control_to_redux_form/src/components/posts_new.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+    render() {
+        const { fields: { title, catgories, content }, handleSubmit } = this.props;
+
+        return (
+            <form onSubmit={handleSubmit}>
+                <h3>Create a New Post</h3>
+                <div className="form-group">
+                    <label>Title</label>
+                    <input type="text" className="form-control" {...title} />
+                </div>
+
+                <div className="form-group">
+                    <label>Categories</label>
+                    <input type="text" className="form-control" {...categories} />
+                </div>
+
+                <div className="form-group">
+                    <label>Content</label>
+                    <textarea className="form-control" {...content} />
+                </div>
+
+                <button type="submit" className="btn btn-primary">
+                    Submit
+                </button>
+            </form>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'PostsNewForm',
+    fields: ['title', 'categories', 'content']
+})(PostsNew);
