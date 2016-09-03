9947fa7ec538dae2c785322e15f9dc9a50230cbf
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-03 14:58:51 +0200

    Added create post action creator

---
+++ JavaScript/ReactJS/modern-react-with-redux/32_create_post_action_creator/src/components/posts_new.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
+
+class PostsNew extends Component {
+    render() {
+        const { fields: { title, categories, content }, handleSubmit } = this.props;
+
+        return (
+            <form onSubmit={handleSubmit(this.props.createPost)}>
+                <h3>Create a New Post</h3>
+                <div className="form-group">
+                    <label>Title</label>
+                    <input type="text" className="form-control" {...title} />
+                </div>
+
+                <div className="form-group">
+                    <label>Categories</label>
+                    <input type="text" className="form-control" {...categories} />
+                </div>
+
+                <div className="form-group">
+                    <label>Content</label>
+                    <textarea className="form-control" {...content} />
+                </div>
+
+                <button type="submit" className="btn btn-primary">
+                    Submit
+                </button>
+            </form>
+        );
+    }
+}
+
+// connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+
+export default reduxForm({
+    form: 'PostsNewForm',
+    fields: ['title', 'categories', 'content']
+}, null, { createPost })(PostsNew);
