149df1108c525c58b49a1022e8b86627c8f5b4e2
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-03 15:27:58 +0200

    Finished form validation

--- JavaScript/ReactJS/modern-react-with-redux/33_form_validation/src/components/posts_new.js
+++ JavaScript/ReactJS/modern-react-with-redux/33_form_validation/src/components/posts_new.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsNew extends Component {
     render() {
@@ -9,27 +10,34 @@ class PostsNew extends Component {
         return (
             <form onSubmit={handleSubmit(this.props.createPost)}>
                 <h3>Create a New Post</h3>
-                <div className="form-group">
+                <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
                     <label>Title</label>
                     <input type="text" className="form-control" {...title} />
-                    <div className="text-help">
+                    <div className="form-control-feedback">
                         {title.touched ? title.error : ''}
                     </div>
                 </div>
 
-                <div className="form-group">
+                <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
                     <label>Categories</label>
                     <input type="text" className="form-control" {...categories} />
+                    <div className="form-control-feedback">
+                        {categories.touched ? categories.error : ''}
+                    </div>
                 </div>
 
-                <div className="form-group">
+                <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
                     <label>Content</label>
                     <textarea className="form-control" {...content} />
+                    <div className="form-control-feedback">
+                        {content.touched ? content.error : ''}
+                    </div>
                 </div>
 
                 <button type="submit" className="btn btn-primary">
-                    Submit
+                    Save
                 </button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
@@ -39,7 +47,13 @@ function validate(values) {
     const errors = {};
 
     if (!values.title) {
-        errors.title = 'Enter a username';
+        errors.title = 'Enter a title';
+    }
+    if (!values.categories) {
+        errors.categories = 'Enter categories';
+    }
+    if (!values.content) {
+        errors.content = 'Enter some content';
     }
 
     return errors;
