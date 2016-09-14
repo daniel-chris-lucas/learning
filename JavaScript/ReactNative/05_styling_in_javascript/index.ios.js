bdb1d836ba557b528aa0c0da3e2b32c3d23e88ee
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-14 23:41:07 +0200

    Added styling in javascript

---
+++ JavaScript/ReactNative/05_styling_in_javascript/index.ios.js
@@ -0,0 +1,32 @@
+// Import some code we need
+var React = require('react-native');
+var AppRegistry = React.AppRegistry;
+var Text = React.Text;
+var View = React.View;
+var StyleSheet = React.StyleSheet;
+
+// Create a react component
+var Weekdays = React.createClass({
+    render: function () {
+        return <View style={styles.container}>
+            <Text>
+                Days of the week:
+            </Text>
+        </View>
+    }
+});
+
+// Style the React component
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        // flexDirection: 'column', // column | row
+        justifyContent: 'center', // flex-start | center | flex-end
+        alignItems: 'center' // flex-start | center | flex-end
+    }
+});
+
+// Show the react component on the screen
+AppRegistry.registerComponent('weekdays', function () {
+    return Weekdays;
+});
