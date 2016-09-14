8feedf3e4bee7c5d2b5fd031d60d08edee293665
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-15 01:03:55 +0200

    Added dynamic styling

---
+++ JavaScript/ReactNative/build-apps-with-react-native/11_dynamic_styling/src/day-item.js
@@ -0,0 +1,37 @@
+// Import some code we need
+var React = require('react-native');
+var Text = React.Text;
+
+// Create our component
+var DayItem = React.createClass({
+    render: function () {
+        return <Text style={this.style()}>
+            {this.props.day}
+        </Text>
+    },
+    style: function () {
+        return {
+            color: this.color(),
+            fontWeight: this.fontWeight(),
+            fontSize: this.fontSize(),
+            lineHeight: this.lineHeight()
+        };
+    },
+    color: function () {
+        var opacity = 1 / (this.props.daysUntil + 1);
+        return 'rgba(0, 0, 0, ' + opacity + ')';
+    },
+    fontWeight: function () {
+        var weight = 7 - this.props.daysUntil;
+        return String(weight * 100);
+    },
+    fontSize: function () {
+        return 60 - 6 * this.props.daysUntil;
+    },
+    lineHeight: function () {
+        return 70 - 4 * this.props.daysUntil;
+    }
+});
+
+// Make this code available elsewhere
+module.exports = DayItem;
