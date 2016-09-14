b007992228e315fbbdbe0e7292cb88616d7ada30
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-15 00:02:05 +0200

    Added composition of components

---
+++ JavaScript/ReactNative/06_composition_of_components/src/day-item.js
@@ -0,0 +1,24 @@
+// Import some code we need
+var React = require('react-native');
+var Text = React.Text;
+var StyleSheet = React.StyleSheet;
+
+// Create our component
+var DayItem = React.createClass({
+    render: function () {
+        return <Text style={styles.day}>
+            A day of the week
+        </Text>
+    }
+});
+
+// Style our component
+var styles = StyleSheet.create({
+    day: {
+        fontSize: 18,
+        color: '#0000FF'
+    }
+});
+
+// Make this code available elsewhere
+module.exports = DayItem;
