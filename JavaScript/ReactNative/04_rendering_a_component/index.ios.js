63cc88ce99130c50f7e7168b86bc1dd825ccd45c
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-14 23:13:49 +0200

    Added rendering a component

---
+++ JavaScript/ReactNative/04_rendering_a_component/index.ios.js
@@ -0,0 +1,21 @@
+// Import some code we need
+var React = require('react-native');
+var AppRegistry = React.AppRegistry;
+var Text = React.Text;
+var View = React.View;
+
+// Create a react component
+var Weekdays = React.createClass({
+    render: function () {
+        return <View>
+            <Text>
+                Days of the week:
+            </Text>
+        </View>
+    }
+});
+
+// Show the react component on the screen
+AppRegistry.registerComponent('weekdays', function () {
+    return Weekdays;
+});
