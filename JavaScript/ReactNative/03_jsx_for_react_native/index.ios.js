4718eb271000e5079612f43d8861cb1fcd9b43de
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-14 23:03:23 +0200

    Added jsx for react native

---
+++ JavaScript/ReactNative/03_jsx_for_react_native/index.ios.js
@@ -0,0 +1,12 @@
+// Create a react component
+var Weekdays = React.createClass({
+    render: function () {
+        return <View>
+            <Text>
+                Days of the week:
+            </Text>
+        </View>
+    }
+});
+
+// Show the react component on the screen
