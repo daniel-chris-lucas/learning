514566be18b3eee83617f6eabbfe44db344bba07
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-15 21:37:56 +0200

    Added stopwatch

---
+++ JavaScript/ReactNative/build-apps-with-react-native/12_stopwatch_init/index.ios.js
@@ -0,0 +1,14 @@
+var React = require('react-native');
+var {
+    Text,
+    View,
+    AppRegistry
+} = React;
+
+var StopWatch = React.createClass({
+    render() {
+
+    }
+});
+
+AppRegistry.registerComponent('stopwatch', () => StopWatch);
