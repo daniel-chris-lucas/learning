ea77fd6a49b4a36958a1d672e5726364dfdb79d4
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-15 22:02:03 +0200

    Added keeping render clean

---
+++ JavaScript/ReactNative/build-apps-with-react-native/13_keeping_render_clean/index.ios.js
@@ -0,0 +1,34 @@
+var React = require('react-native');
+var {
+    Text,
+    View,
+    AppRegistry
+} = React;
+
+var StopWatch = React.createClass({
+    render: function () {
+        return <View>
+            <Text>
+                00:00:00
+            </Text>
+            {this.startStopButton()}
+            {this.lapButton()}
+        </View>
+    },
+    startStopButton: function () {
+        return <View>
+            <Text>
+                Start
+            </Text>
+        </View>
+    },
+    lapButton: function () {
+        return <View>
+            <Text>
+                Lap
+            </Text>
+        </View>
+    }
+});
+
+AppRegistry.registerComponent('stopwatch', () => StopWatch);
