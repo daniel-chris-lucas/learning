6a2ca1a06cb2e68b59af034400a28ab33894fbb9
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-16 18:08:26 +0200

    Added network requests

---
+++ JavaScript/ReactNative/build-apps-with-react-native/25_network_requests/index.ios.js
@@ -0,0 +1,41 @@
+var React = require('react-native');
+var {
+    AppRegistry,
+    MapView,
+    View,
+    StyleSheet
+} = React;
+
+var Weather = React.createClass({
+    getInitialState: function () {
+        return {
+            pin: {
+                latitude: 0,
+                longitude: 0
+            }
+        };
+    },
+    render: function () {
+        return <MapView
+            annotations={[this.state.pin]}
+            onRegionChangeComplete={this.onRegionChangeComplete}
+            style={styles.map}
+        ></MapView>
+    },
+    onRegionChangeComplete: function (region) {
+        this.setState({
+            pin: {
+                longitude: region.longitude,
+                latitude: region.latitude
+            }
+        });
+    }
+});
+
+var styles = StyleSheet.create({
+    map: {
+        flex: 1
+    }
+});
+
+AppRegistry.registerComponent('weather', () => Weather);
---
+++ JavaScript/ReactNative/build-apps-with-react-native/25_network_requests/src/api.js
@@ -0,0 +1,21 @@
+var rootUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=cc8c06050986c92e450204a5dc60e99f';
+
+var kelvinToC = function (kelvin) {
+    return Math.round(kelvin - 273.15) + ' °C';
+};
+
+module.exports = function (latitude, longitude) {
+    var url = `${rootUrl}&lat=${latitude}&lon=${longitude}`;
+
+    fetch(url)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (json) {
+            return {
+                city: json.name,
+                temperature: kelvinToC(json.main.temp),
+                description: json.weather[0].description
+            };
+        });
+};
