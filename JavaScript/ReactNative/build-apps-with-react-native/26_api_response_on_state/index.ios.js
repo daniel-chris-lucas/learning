c1d1c4683765cd4333ba577abda0c2d445c95f11
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-16 18:36:07 +0200

    Added Api response on state

---
+++ JavaScript/ReactNative/build-apps-with-react-native/26_api_response_on_state/index.ios.js
@@ -0,0 +1,52 @@
+var React = require('react-native');
+var {
+    AppRegistry,
+    MapView,
+    View,
+    StyleSheet
+} = React;
+
+var Api = require('./src/api');
+
+var Weather = React.createClass({
+    getInitialState: function () {
+        return {
+            pin: {
+                latitude: 0,
+                longitude: 0
+            },
+            city: '',
+            temperature: '',
+            description: ''
+        };
+    },
+    render: function () {
+        return <MapView
+            annotations={[this.state.pin]}
+            onRegionChangeComplete={this.onRegionChangeComplete}
+            style={styles.map}
+        ></MapView>
+    },
+    onRegionChangeComplete: function (region) {
+        this.setState({
+            pin: {
+                longitude: region.longitude,
+                latitude: region.latitude
+            }
+        });
+
+        Api(region.latitude, region.longitude)
+            .then((data) => {
+                console.log(data);
+                this.setState(data);
+            });
+    }
+});
+
+var styles = StyleSheet.create({
+    map: {
+        flex: 1
+    }
+});
+
+AppRegistry.registerComponent('weather', () => Weather);
