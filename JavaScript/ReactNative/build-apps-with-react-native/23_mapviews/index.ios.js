e65f50c25dbb2242619f60fcb58864a767f914fe
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-16 17:35:28 +0200

    Added mapviews

---
+++ JavaScript/ReactNative/build-apps-with-react-native/23_mapviews/index.ios.js
@@ -0,0 +1,33 @@
+var React = require('react-native');
+var {
+    AppRegistry,
+    MapView,
+    View,
+    StyleSheet
+} = React;
+
+var Weather = React.createClass({
+    render: function () {
+        var pins = [{
+            latitude: 37,
+            longitude: -95
+        }];
+
+        return <MapView
+            annotations={pins}
+            onRegionChangeComplete={this.onRegionChangeComplete}
+            style={styles.map}
+        ></MapView>
+    },
+    onRegionChangeComplete: function (region) {
+        console.log(region);
+    }
+});
+
+var styles = StyleSheet.create({
+    map: {
+        flex: 1
+    }
+});
+
+AppRegistry.registerComponent('weather', () => Weather);
