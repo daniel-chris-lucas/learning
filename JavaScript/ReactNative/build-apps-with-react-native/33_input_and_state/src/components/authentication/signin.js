589c656ad5a739834b0deb3cd9a010a3755ba9c5
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-18 00:19:14 +0200

    Added input and state

---
+++ JavaScript/ReactNative/build-apps-with-react-native/33_input_and_state/src/components/authentication/signin.js
@@ -0,0 +1,68 @@
+var React = require('react-native');
+var {
+    View,
+    Text,
+    StyleSheet,
+    TextInput
+} = React;
+
+var Button = require('../common/button');
+
+module.exports = React.createClass({
+    getInitialState: function () {
+        return {
+            username: '',
+            password: ''
+        };
+    },
+
+    render: function () {
+        return (
+            <View style={styles.container}>
+                <Text>Sign In</Text>
+
+                <Text style={styles.label}>Username:</Text>
+                <TextInput
+                    style={styles.input}
+                    value={this.state.username}
+                    onChangeText={(text) => this.setState({username: text})}
+                />
+
+                <Text style={styles.label}>Password:</Text>
+                <TextInput
+                    secureTextEntry={true}
+                    style={styles.input}
+                    value={this.state.password}
+                    onChangeText={(text) => this.setState({password: text})}
+                />
+                <Button text={'Sign In'} onPress={this.onPress} />
+            </View>
+        );
+    },
+    onPress: function () {
+        this.setState({
+            password: ''
+        });
+    }
+});
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    input: {
+        padding: 4,
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 5,
+        margin: 5,
+        width: 200,
+        alignSelf: 'center'
+    },
+    label: {
+        fontSize: 18
+    }
+});
