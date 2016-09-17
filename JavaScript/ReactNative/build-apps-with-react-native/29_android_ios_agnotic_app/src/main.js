e6540b85ee0f36435c9ccbb564dac0f2433ebecb
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-17 17:19:24 +0200

    Added android ios agnostic appé

---
+++ JavaScript/ReactNative/build-apps-with-react-native/29_android_ios_agnotic_app/src/main.js
@@ -0,0 +1,24 @@
+var React = require('react-native');
+var {
+    View,
+    Text,
+    StyleSheet
+} = React;
+
+module.exports = React.createClass({
+    render: function () {
+        return (
+            <View style={styles.container}>
+                <Text>I am on both iOS and Android!</Text>
+            </View>
+        );
+    }
+});
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
