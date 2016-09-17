723383a6c491f42423fb9fdded720d8a5a7d4cc3
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-17 19:46:55 +0200

    Added authentication setup

---
+++ JavaScript/ReactNative/build-apps-with-react-native/30_authentication_setup/src/components/authentication/signin.js
@@ -0,0 +1,25 @@
+var React = require('react-native');
+var {
+    View,
+    Text,
+    StyleSheet,
+    TextInput
+} = React;
+
+module.exports = React.createClass({
+    render: function () {
+        return (
+            <View style={styles.container}>
+                <Text>Sign In</Text>
+            </View>
+        );
+    }
+});
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
