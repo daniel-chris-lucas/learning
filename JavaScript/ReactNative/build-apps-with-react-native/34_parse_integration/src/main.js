57723eea1736840dc10a8b8869598ce6450fd497
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-18 02:44:40 +0200

    Added albums

---
+++ JavaScript/ReactNative/build-apps-with-react-native/34_parse_integration/src/main.js
@@ -0,0 +1,26 @@
+var React = require('react-native');
+var {
+    View,
+    Text,
+    StyleSheet
+} = React;
+
+var Signin = require('./components/authentication/signin');
+
+module.exports = React.createClass({
+    render: function () {
+        return (
+            <View style={styles.container}>
+                <Signin />
+            </View>
+        );
+    }
+});
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
