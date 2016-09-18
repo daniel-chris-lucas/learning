b538b5ea50f99327842814edbb425523790b1f37
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-18 16:01:12 +0200

    Added component level state

---
+++ JavaScript/ReactNative/react_native_and_redux/07_network_requests/src/components/AlbumList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+class AlbumList extends Component {
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => console.log(response));
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>Album List!!!</Text>
+            </View>
+        );
+    }
+}
+
+export default AlbumList;
---
+++ JavaScript/ReactNative/react_native_and_redux/08_component_level_state/src/components/AlbumList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+class AlbumList extends Component {
+    state = { albums: [] };
+
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data }));
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>Album List!!!</Text>
+            </View>
+        );
+    }
+}
+
+export default AlbumList;
