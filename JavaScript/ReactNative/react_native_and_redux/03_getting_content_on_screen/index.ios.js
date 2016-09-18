46bf19053ee6ff24584588e0282c4200bcac3c2c
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-18 04:22:56 +0200

    Added getting content on screen

---
+++ JavaScript/ReactNative/react_native_and_redux/03_getting_content_on_screen/index.ios.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => (
+    <Text>Some Text</Text>
+);
+
+AppRegistry.registerComponent('albums', () => App);
