729abb74249f39ee54854b3cf4b2f2d649ad2381
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-18 23:08:08 +0200

    Added creating an activity spinner

---
+++ JavaScript/ReactNative/react_native_and_redux/28_creating_an_activity_spinner/src/components/LoginForm.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
+import { Button, Card, CardSection, Input } from './common';
+
+class LoginForm extends Component {
+    state = { email: '', password: '', error: '' };
+
+    onButtonPress() {
+        const { email, password } = this.state;
+
+        this.setState({ error: '' });
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .catch(() => {
+                        this.setState({ error: 'Authentication Failed.' });
+                    });
+            });
+    }
+
+    render() {
+        return (
+            <Card>
+                <CardSection>
+                    <Input
+                        placeholder="user@gmail.com"
+                        label="Email"
+                        value={this.state.email}
+                        onChangeText={email => this.setState({ email })}
+                    />
+                </CardSection>
+
+                <CardSection>
+                    <Input
+                        secureTextEntry
+                        placeholder="password"
+                        label="Password"
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                    />
+                </CardSection>
+
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>
+
+                <CardSection>
+                    <Button onPress={this.onButtonPress.bind(this)}>
+                        Log in
+                    </Button>
+                </CardSection>
+            </Card>
+        );
+    }
+}
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+};
+
+export default LoginForm;
