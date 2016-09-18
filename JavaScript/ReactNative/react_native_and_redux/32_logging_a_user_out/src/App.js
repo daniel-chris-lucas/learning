5e067643e0d99149c21dbfc6d2e3aaab60a9c12a
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-19 00:16:12 +0200

    Added logging a user out

---
+++ JavaScript/ReactNative/react_native_and_redux/32_logging_a_user_out/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+    state = { loggedIn: null };
+
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyA13TR8zdSfwmdH-FUPkgyIDnP_IxqWvB8',
+            authDomain: 'authentication-99ddc.firebaseapp.com',
+            databaseURL: 'https://authentication-99ddc.firebaseio.com',
+            storageBucket: 'authentication-99ddc.appspot.com',
+            messagingSenderId: '929891825634'
+        });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <Button onPress={() => firebase.auth().signOut()}>
+                        Log Out
+                    </Button>
+                );
+            case false:
+                return <LoginForm />;
+            default:
+                return <Spinner size="large" />;
+        }
+    }
+
+    render() {
+        return (
+            <View>
+                <Header>Authentication</Header>
+                {this.renderContent()}
+            </View>
+        );
+    }
+}
+
+export default App;
