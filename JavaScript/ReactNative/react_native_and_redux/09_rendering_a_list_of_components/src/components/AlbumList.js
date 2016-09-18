7b8c6e2b1adff8f84e21edd00b13c2e5cd1381b1
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-18 16:07:29 +0200

    Added rendering a list of components

---
+++ JavaScript/ReactNative/react_native_and_redux/09_rendering_a_list_of_components/src/components/AlbumList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+class AlbumList extends Component {
+    state = { albums: [] };
+
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data }));
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album => <Text>{album.title}</Text>);
+    }
+
+    render() {
+        return (
+            <View>
+                {this.renderAlbums()}
+            </View>
+        );
+    }
+}
+
+export default AlbumList;
