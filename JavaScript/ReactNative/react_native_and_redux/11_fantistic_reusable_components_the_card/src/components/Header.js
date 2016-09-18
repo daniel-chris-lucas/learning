9014761db2eeadba53e8727c11e9bda4518f7ad9
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-18 16:23:12 +0200

    Added card component

---
+++ JavaScript/ReactNative/react_native_and_redux/11_fantistic_reusable_components_the_card/src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.children}</Text>
+        </View>
+    );
+};
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#f8f8f8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2
+    },
+    textStyle: {
+        fontSize: 20
+    }
+};
+
+export default Header;
