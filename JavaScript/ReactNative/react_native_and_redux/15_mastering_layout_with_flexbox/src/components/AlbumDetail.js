de8da1846aeacede92afaf26d7e1e3647d4eee8b
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-18 17:09:21 +0200

    Added mastering layout with flexbox

---
+++ JavaScript/ReactNative/react_native_and_redux/15_mastering_layout_with_flexbox/src/components/AlbumDetail.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image } = album;
+    const { thumbnailStyle, headerContentStyle } = styles;
+
+    return (
+        <Card>
+            <CardSection>
+                <View>
+                    <Image
+                        style={thumbnailStyle}
+                        source={{ uri: thumbnail_image }}
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+        </Card>
+    );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    }
+};
+
+export default AlbumDetail;
