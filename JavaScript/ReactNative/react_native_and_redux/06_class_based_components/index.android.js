b872e0e73974a44d914ed1a235d1a09e17967ecf
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-18 15:35:43 +0200

    Added class based components

---
+++ JavaScript/ReactNative/react_native_and_redux/06_class_based_components/index.android.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+const App = () => (
+    <View>
+        <Header>Albums</Header>
+        <AlbumList />
+    </View>
+);
+
+AppRegistry.registerComponent('albums', () => App);
