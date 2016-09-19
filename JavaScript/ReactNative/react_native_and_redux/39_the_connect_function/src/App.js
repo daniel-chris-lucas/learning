296b86994951ab600f604a9b2f16046639652cce
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-19 23:35:54 +0200

    Added the connect function

---
+++ JavaScript/ReactNative/react_native_and_redux/39_the_connect_function/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Header } from './components/common';
+
+
+const App = () => {
+    return (
+        <Provider store={createStore(reducers)}>
+            <View>
+                <Header>Tech Stack</Header>
+            </View>
+        </Provider>
+    );
+};
+
+export default App;
