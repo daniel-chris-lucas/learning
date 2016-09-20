6c574248bf73a1cc3f8d1c155d647577948d0c08
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-20 23:02:14 +0200

    Added expanding a row

---
+++ JavaScript/ReactNative/react_native_and_redux/48_expanding_a_row/src/components/ListItem.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
+import { CardSection } from './common';
+import * as actions from '../actions';
+
+class ListItem extends Component {
+    renderDescription() {
+        const { library, selectedLibraryId } = this.props;
+
+        if (library.id === selectedLibraryId) {
+            return (
+                <Text>{library.description}</Text>
+            );
+        }
+    }
+
+    render() {
+        const { titleStyle } = styles;
+        const { id, title } = this.props.library;
+
+        return (
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>
+                            {title}
+                        </Text>
+                    </CardSection>
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    }
+}
+
+const styles = {
+    titleStyle: {
+        fontSize: 18,
+        paddingLeft: 15
+    }
+};
+
+const mapStateToProps = state => {
+    return { selectedLibraryId: state.selectedLibraryId };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
