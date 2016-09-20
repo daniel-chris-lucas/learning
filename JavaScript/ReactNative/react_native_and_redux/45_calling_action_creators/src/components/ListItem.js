49d5a11b513a796e4550c989bb9ff81d798dcb11
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-20 22:01:33 +0200

    Added calling action creators

---
+++ JavaScript/ReactNative/react_native_and_redux/45_calling_action_creators/src/components/ListItem.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { connect } from 'react-redux';
+import { CardSection } from './common';
+import * as actions from '../actions';
+
+class ListItem extends Component {
+    render() {
+        const { titleStyle } = styles;
+
+        return (
+            <CardSection>
+                <Text style={titleStyle}>
+                    {this.props.library.title}
+                </Text>
+            </CardSection>
+        );
+    }
+}
+
+const styles = {
+    titleStyle: {
+        fontSize: 18,
+        paddingLeft: 15
+    }
+};
+
+export default connect(null, actions)(ListItem);
