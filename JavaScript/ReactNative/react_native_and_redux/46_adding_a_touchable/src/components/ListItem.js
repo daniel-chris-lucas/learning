e768183f9297fd8ec31a5cbfbff6609d1a829792
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-20 22:31:12 +0200

    Added adding a touchable

---
+++ JavaScript/ReactNative/react_native_and_redux/46_adding_a_touchable/src/components/ListItem.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
+import { CardSection } from './common';
+import * as actions from '../actions';
+
+class ListItem extends Component {
+    render() {
+        const { titleStyle } = styles;
+        const { id, title } = this.props.library;
+
+        return (
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>
+                            {title}
+                        </Text>
+                    </CardSection>
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    }
+}
+
+const styles = {
+    titleStyle: {
+        fontSize: 18,
+        paddingLeft: 15
+    }
+};
+
+export default connect(null, actions)(ListItem);
