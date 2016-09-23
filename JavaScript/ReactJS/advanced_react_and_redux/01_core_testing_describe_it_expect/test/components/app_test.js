bec957df35139ff00208a75557018929e0cd31e7
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-24 01:37:57 +0200

    Added Core testing - describe, it and expect

---
+++ JavaScript/ReactJS/advanced_react_and_redux/01_core_testing_describe_it_expect/test/components/app_test.js
@@ -0,0 +1,11 @@
+import { renderComponent, expect } from '../test_helper';
+import App from '../../src/components/app';
+
+// Use 'describe' to group together similar tests
+describe('App')
+
+// Use 'it' to test a single attribute of a target
+it('shows the correct text')
+
+// Use 'expect' to make an assertion about a target
+expect
