d3d5162e54501fe42cfd97848ad01e991875e861
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-24 20:01:35 +0200

    Added assertions with lists

---
+++ JavaScript/ReactJS/advanced_react_and_redux/13_assertions_with_lists/src/components/comment_box.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+export default class CommentBox extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { comment: '' };
+    }
+
+    handleChange(event) {
+        this.setState({ comment: event.target.value });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        this.setState({ comment: '' });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit.bind(this)} className="comment-box">
+                <textarea
+                    value={this.state.comment}
+                    onChange={this.handleChange.bind(this)}
+                />
+
+                <button type="submit">
+                    Submit Comment
+                </button>
+            </form>
+        );
+    }
+};
