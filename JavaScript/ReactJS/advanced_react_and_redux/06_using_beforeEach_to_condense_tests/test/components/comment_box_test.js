af22a99566c35324a7c1d6867c2926e645486075
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-24 17:03:18 +0200

    Added using beforeEach to condense tests

---
+++ JavaScript/ReactJS/advanced_react_and_redux/06_using_beforeEach_to_condense_tests/test/components/comment_box_test.js
@@ -0,0 +1,22 @@
+import { renderComponent, expect } from '../test_helper';
+import CommentBox from '../../src/components/comment_box';
+
+describe('CommentBox', () => {
+    let component;
+
+    beforeEach(() => {
+        component = renderComponent(CommentBox);
+    });
+
+    it('has the correct class', () => {
+        expect(component).to.have.class('comment-box');
+    });
+
+    it('has a text area', () => {
+        expect(component.find('textarea')).to.exist;
+    });
+
+    it('has a button', () => {
+        expect(component.find('button')).to.exist;
+    });
+});
