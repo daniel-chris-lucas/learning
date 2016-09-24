36471d771a53af85e5c5202e5393d1b4ad3acf0d
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-24 16:37:40 +0200

    Updated comment box tests

--- JavaScript/ReactJS/advanced_react_and_redux/03_test_structure_setup/test/components/comment_box_test.js
+++ JavaScript/ReactJS/advanced_react_and_redux/03_test_structure_setup/test/components/comment_box_test.js
@@ -1,2 +1,12 @@
 import { renderComponent, expect } from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
+
+describe('CommentBox', () => {
+    it('has a text area', () => {
+
+    });
+
+    it('has a button', () => {
+
+    });
+});
