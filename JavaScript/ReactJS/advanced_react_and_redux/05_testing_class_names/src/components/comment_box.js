6ed1a763ae4c15587380bec60ac7d5fffe3e4ce1
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-24 16:55:10 +0200

    Using beforeEach to condense tests

---
+++ JavaScript/ReactJS/advanced_react_and_redux/05_testing_class_names/src/components/comment_box.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class CommentBox extends Component {
+    render() {
+        return (
+            <div className="comment-box">
+                <textarea />
+                <button>Submit Comment</button>
+            </div>
+        );
+    }
+};
