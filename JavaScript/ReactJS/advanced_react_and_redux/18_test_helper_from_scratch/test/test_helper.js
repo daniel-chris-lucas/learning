0af13f90ba5a5fd166dd5b71c8105c5d6f8b12d2
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-25 14:12:42 +0200

    Added test helper from scratch

---
+++ JavaScript/ReactJS/advanced_react_and_redux/18_test_helper_from_scratch/test/test_helper.js
@@ -0,0 +1,15 @@
+
+
+// Set up testing environment to run like a browser in the command line
+
+
+
+// Build 'renderComponent' helper that should render a given react class
+
+
+
+// Build helper for simulating events
+
+
+
+// Set up chai-jquery
