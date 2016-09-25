aedf2ad92f9daaf5639862a172384c5a054661b8
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-25 21:48:31 +0200

    Added authentication higher order component

---
+++ JavaScript/ReactJS/advanced_react_and_redux/30_authentication_hoc/src/components/require_authentication.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default function(ComposedComponent) {
+    class Authentication extends Component {
+        render() {
+            return <ComposedComponent {...this.props} />;
+        }
+    }
+
+    return Authentication;
+}
