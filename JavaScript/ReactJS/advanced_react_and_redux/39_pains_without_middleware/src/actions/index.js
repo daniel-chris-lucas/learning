cdd69aee3cfad3ff87c368284222d0b7fcdad741
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-27 22:51:43 +0200

    Added pains without middleware

---
+++ JavaScript/ReactJS/advanced_react_and_redux/39_pains_without_middleware/src/actions/index.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { FETCH_USERS } from './types';
+
+export function fetchUsers() {
+    const request = axios.get('http://jsonplaceholder.typicode.com/users')
+    .then((response) => {
+        return {
+            type: FETCH_USERS,
+            payload: response.data
+        };
+    });
+}
