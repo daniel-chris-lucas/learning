64d1186bd22a405ee50f54914436d832627f8187
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-09-28 00:05:22 +0200

    Added handling promises

---
+++ JavaScript/ReactJS/advanced_react_and_redux/42_handling_promises/src/actions/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import { FETCH_USERS } from './types';
+
+export function fetchUsers() {
+    const request = axios.get('http://jsonplaceholder.typicode.com/users');
+
+    return {
+        type: FETCH_USERS,
+        payload: request
+    };
+}
---
+++ JavaScript/ReactJS/advanced_react_and_redux/42_handling_promises/src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import UserList from './user_list';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+          <UserList />
+      </div>
+    );
+  }
+}
