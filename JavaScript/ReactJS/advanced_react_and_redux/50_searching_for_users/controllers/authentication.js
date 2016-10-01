b6278e2ef2b95dad99dbe916ba820a91a43e3a53
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-10-01 17:25:16 +0200

    Added searching for users

---
+++ JavaScript/ReactJS/advanced_react_and_redux/50_searching_for_users/controllers/authentication.js
@@ -0,0 +1,17 @@
+const User = require('../models/user');
+
+exports.signup = function (req, res, next) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // See if a user with the given email exists
+    User.findOne({ email: email }, function (err, existingUser) {
+        
+    });
+
+    // If a user with email foes exist, return an error
+
+    // If a user with email does NOT exist, create and save user record
+
+    // Respond to request indicating the user was created
+};
