fb36b817b4e62565ed1e4a0ad74f430294ce44c9
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-10-01 18:40:56 +0200

    Added comments

--- JavaScript/ReactJS/advanced_react_and_redux/52_encrypting_passwords_with_bcrypt/models/user.js
+++ JavaScript/ReactJS/advanced_react_and_redux/52_encrypting_passwords_with_bcrypt/models/user.js
@@ -9,15 +9,20 @@ const userSchema = new Schema({
 });
 
 // On save hook, encrypt password
+// Before saving a model, run this function
 userSchema.pre('save', function (next) {
+    // get access to the user model
     const user = this;
 
+    // generate a salt then run callback
     bcrypt.genSalt(10, function (err, salt) {
         if (err) { return next(err); }
 
+        // hash (encrypt) our password using the salt
         bcrypt.hash(user.password, salt, null, function (err, hash) {
             if (err) { return next(err); }
 
+            // overwrite plain text password with encrypted password
             user.password = hash;
             next();
         });
