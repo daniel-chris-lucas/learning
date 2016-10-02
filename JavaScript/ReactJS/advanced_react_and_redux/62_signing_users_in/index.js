6c3161cc6879a963dae0692101ec38c6a699bdc8
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-10-02 16:27:18 +0200

    Added signing users in

---
+++ JavaScript/ReactJS/advanced_react_and_redux/62_signing_users_in/index.js
@@ -0,0 +1,23 @@
+// Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB Setup
+const dbName = 'auth';
+mongoose.connect('mongodb://localhost:auth/' + dbName);
+
+// App setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+// Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on: ', port);
