ac6931bd700d701df8df9f4521a7b0bd46110b45
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-10-04 00:48:40 +0200

    Added displaying errors

---
+++ JavaScript/ReactJS/advanced_react_and_redux/76_displaying_errors/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import reduxThunk from 'redux-thunk';
+
+import App from './components/app';
+import Signin from './components/auth/signin';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <Route path="signin" component={Signin} />
+        </Route>
+    </Router>
+  </Provider>
+  , document.querySelector('.container'));
