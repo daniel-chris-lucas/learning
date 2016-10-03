30d5771283c5137ee46a3fb0c57ed40095f35085
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-10-04 00:35:56 +0200

    Added localstorage and jwt

---
+++ JavaScript/ReactJS/advanced_react_and_redux/74_localstorage_and_jwt/src/actions/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
+
+const ROOT_URL = 'http://localhost:3090';
+
+export function signinUser({ email, password }) {
+    return function (dispatch) {
+        // Submit email/password to the server
+        axios.post(`${ROOT_URL}/signin`, { email, password })
+            .then(response => {
+                // If request is good...
+                // - Update state to indicate user is authenticated
+                dispatch({ type: AUTH_USER });
+                // - Save the JWT token
+                localStorage.setItem('token', response.data.token);
+                // -redirect to the route '/feature'
+                browserHistory.push('/feature');
+            })
+            .catch(() => {
+                // If request is bad...
+                // - Show an error the the user
+            });
+    }
+}
