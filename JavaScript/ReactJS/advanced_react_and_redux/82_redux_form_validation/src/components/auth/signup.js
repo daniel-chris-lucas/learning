549a7085e04b07633d15fc4edb0073a67d42c977
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-10-04 22:55:37 +0200

    More validation

--- JavaScript/ReactJS/advanced_react_and_redux/82_redux_form_validation/src/components/auth/signup.js
+++ JavaScript/ReactJS/advanced_react_and_redux/82_redux_form_validation/src/components/auth/signup.js
@@ -11,6 +11,7 @@ class Signup extends Component {
                 <fieldset className="form-group">
                     <label>Email:</label>
                     <input className="form-control" {...email} />
+                    {email.touched && email.error && <div className="error">{email.error}</div>}
                 </fieldset>
                 <fieldset className="form-group">
                     <label>Password:</label>
@@ -20,6 +21,7 @@ class Signup extends Component {
                 <fieldset className="form-group">
                     <label>Confirm Password:</label>
                     <input className="form-control" {...passwordConfirm} type="password" />
+                    {passwordConfirm.touched && passwordConfirm.error && <div className="error">{passwordConfirm.error}</div>}
                 </fieldset>
                 <button type="submit" className="btn btn-primary">Sign up!</button>
             </form>
@@ -29,6 +31,18 @@ class Signup extends Component {
 
 function validate(formProps) {
     const errors = {};
+
+    if (!formProps.email) {
+        errors.email = 'Please enter an email';
+    }
+
+    if (!formProps.password) {
+        errors.password = 'Please enter a password';
+    }
+
+    if (!formProps.passwordConfirm) {
+        errors.passwordConfirm = 'Please enter a password confirmation';
+    }
 
     if (formProps.password !== formProps.passwordConfirm) {
         errors.password = 'Passwords must match';
