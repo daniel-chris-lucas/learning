c61fcf5f20244ac3e06086507573febd33b1c64b
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-10-04 22:10:19 +0200

    Added signup component

---
+++ JavaScript/ReactJS/advanced_react_and_redux/80_signup_component/src/components/auth/signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import * as actions from '../../actions';
+
+class Signup extends Component {
+    render() {
+        return (
+            <div>Signup</div>
+        );
+    }
+}
+
+export default Signup
