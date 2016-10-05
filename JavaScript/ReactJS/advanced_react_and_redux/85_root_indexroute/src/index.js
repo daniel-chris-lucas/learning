f8047c3f1c898619043c63c7e26f557821bfa007
daniel-chris-lucas <daniel.chris.lucas@gmail.com>
2016-10-05 21:40:37 +0200

    Added root indexroute

---
+++ JavaScript/ReactJS/advanced_react_and_redux/85_root_indexroute/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import reduxThunk from 'redux-thunk';
+
+import App from './components/app';
+import Signin from './components/auth/signin';
+import Signout from './components/auth/signout';
+import Signup from './components/auth/signup';
+import Feature from './components/feature';
+import RequireAuth from './components/auth/require_auth';
+import Welcome from './components/welcome';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Welcome} />
+            <Route path="signin" component={Signin} />
+            <Route path="signout" component={Signout} />
+            <Route path="signup" component={Signup} />
+            <Route path="feature" component={RequireAuth(Feature)} />
+        </Route>
+    </Router>
+  </Provider>
+  , document.querySelector('.container'));
