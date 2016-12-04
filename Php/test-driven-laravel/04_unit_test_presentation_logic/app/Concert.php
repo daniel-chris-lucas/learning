b17af2439c1e0e6b498be3de79dd306c06340163
Daniel Lucas <daniel.chris.lucas@gmail.com>
2016-12-04 02:31:18 +0100

    Added unit testing presentation logic

---
+++ Php/test-driven-laravel/04_unit_test_presentation_logic/app/Concert.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Concert extends Model
+{
+    protected $guarded = [];
+    protected $dates = ['date'];
+
+    public function getFormattedDateAttribute()
+    {
+        return $this->date->format('F j, Y');
+    }
+}
