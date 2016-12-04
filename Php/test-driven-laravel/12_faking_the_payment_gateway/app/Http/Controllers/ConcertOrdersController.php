a26cef72a82fc9be45c147e13d2c9bc820d4d2a2
Daniel Lucas <daniel.chris.lucas@gmail.com>
2016-12-04 20:01:46 +0100

    Added faking the payment gateway

---
+++ Php/test-driven-laravel/12_faking_the_payment_gateway/app/Http/Controllers/ConcertOrdersController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Billing\PaymentGateway;
+use App\Concert;
+use Illuminate\Http\Request;
+
+class ConcertOrdersController extends Controller
+{
+    private $paymentGateway;
+
+    public function __construct(PaymentGateway $paymentGateway)
+    {
+        $this->paymentGateway = $paymentGateway;
+    }
+
+    public function store($concertId)
+    {
+        $concert = Concert::find($concertId);
+        $ticketQuantity = request('ticket_quantity');
+        $amount = $ticketQuantity * $concert->ticket_price;
+        $token = request('payment_token');
+        $this->paymentGateway->charge($amount, $token);
+        return response()->json([], 201);
+    }
+}
