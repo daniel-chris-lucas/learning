cba31450bd5c40c15ed4acf9ab3b154e14e6f358
Daniel Lucas <daniel.chris.lucas@gmail.com>
2016-12-05 00:55:48 +0100

    Added getting started with validation testing

---
+++ Php/test-driven-laravel/15_validation_testing/app/Http/Controllers/ConcertOrdersController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Billing\PaymentGateway;
+use App\Concert;
+use Illuminate\Http\Request;
+
+class ConcertOrdersController extends Controller
+{
+    private $paymentGateway;
+
+    public function __construct(PaymentGateway $paymentGateway)
+    {
+        $this->paymentGateway = $paymentGateway;
+    }
+
+    public function store($concertId)
+    {
+        $this->validate(request(), [
+            'email' => 'required'
+        ]);
+
+        $concert = Concert::find($concertId);
+        $this->paymentGateway->charge(request('ticket_quantity') * $concert->ticket_price, request('payment_token'));
+        $order = $concert->orderTickets(request('email'), request('ticket_quantity'));
+
+        return response()->json([], 201);
+    }
+}
