a12614c378560edfbf135df064f6712c28774f19
Daniel Lucas <daniel.chris.lucas@gmail.com>
2016-12-04 20:15:36 +0100

    Added adding tickets to orders

---
+++ Php/test-driven-laravel/13_adding_tickets_to_orders/tests/features/PurchaseTicketsTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Billing\FakePaymentGateway;
+use App\Billing\PaymentGateway;
+use App\Concert;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class PurchaseTicketsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function customer_can_purchase_concert_tickets()
+    {
+        $paymentGateway = new FakePaymentGateway;
+        $this->app->instance(PaymentGateway::class, $paymentGateway);
+
+        // Arrange
+        // Create a concert
+        $concert = factory(Concert::class)->create(['ticket_price' => 3250]);
+
+        // Act
+        // Purchase concert tickets
+        $this->json('POST', "/concerts/{$concert->id}/orders", [
+            'email' => 'john@example.com',
+            'ticket_quantity' => 3,
+            'payment_token' => $paymentGateway->getValidTestToken(),
+        ]);
+
+        // Assert
+        $this->assertResponseStatus(201);
+        // Make sure the customer was charged the correct amount
+        $this->assertEquals(9750, $paymentGateway->totalCharges());
+
+        // Make sure that an order exists for the customer
+        $order = $concert->orders()->where('email', 'john@example.com')->first();
+        $this->assertNotNull($order);
+        $this->assertEquals(3, $order->tickets()->count());
+    }
+}
