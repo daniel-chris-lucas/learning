e03f797cb1568672550e734e72a41b4c521eb3d4
Daniel Lucas <daniel.chris.lucas@gmail.com>
2016-12-05 23:20:32 +0100

    Added handling failed charges

---
+++ Php/test-driven-laravel/17_handling_failed_charges/app/Http/Controllers/ConcertOrdersController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Billing\PaymentFailedException;
+use App\Billing\PaymentGateway;
+use App\Concert;
+use Illuminate\Http\Request;
+
+class ConcertOrdersController extends Controller
+{
+    private $paymentGateway;
+
+    public function __construct(PaymentGateway $paymentGateway)
+    {
+        $this->paymentGateway = $paymentGateway;
+    }
+
+    public function store($concertId)
+    {
+        $this->validate(request(), [
+            'email' => ['required', 'email'],
+            'ticket_quantity' => ['required', 'integer',  'min:1'],
+            'payment_token' => ['required'],
+        ]);
+
+        try {
+            $concert = Concert::find($concertId);
+            $this->paymentGateway->charge(request('ticket_quantity') * $concert->ticket_price, request('payment_token'));
+            $order = $concert->orderTickets(request('email'), request('ticket_quantity'));
+
+            return response()->json([], 201);
+        } catch (PaymentFailedException $e) {
+            return response()->json([], 422);
+        }
+    }
+}
