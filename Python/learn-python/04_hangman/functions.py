7bc5b96b72ec4f80f13feb4bbfb2ddbc859ef9ba
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-01-26 00:27:13 +0100

    hangman

--- Python/learn-python/04_hangman/functions.py
+++ Python/learn-python/04_hangman/functions.py
@@ -3,8 +3,10 @@
 Uses the program data from data.py"""
 import os
 import pickle
+from random import choice
 
 from data import *
+
 
 # Score management
 
@@ -15,13 +17,24 @@ def fetch_scores():
 
     if os.path.exists(scores_file_name):
         scores_file = open(scores_file_name, "rb")
-        file_unpickler = pickle.Unpickler(scores_file_name)
+        file_unpickler = pickle.Unpickler(scores_file)
         scores = file_unpickler.load()
         scores_file.close()
     else:
         scores = {}
 
     return scores
+
+
+def save_scores(scores):
+    """Function to save the scores in the file scores_file_name.
+    Receives the scores to save as a parameter"""
+
+    scores_file = open(scores_file_name, "wb")
+    file_pickler = pickle.Pickler(scores_file)
+    file_pickler.dump(scores)
+    scores_file.close()
+
 
 # User input management
 
@@ -34,11 +47,52 @@ def fetch_username():
     until a valid name is given"""
 
     username = input("Enter your name : ")
-    # Put first letter uppercase, the rest lowercase
     username = username.capitalize()
     if not username.isalnum() or len(username) < 4:
-        print("Invalid username.")
-        # Call the function again to enter a new username
+        print("Name is invalid. Please enter another name.")
+        # Call the function again to get another name
         return fetch_username()
-    else:
-        return username
+
+    return username
+
+
+def fetch_letter():
+    """Function for getting a letter from the user.
+    If the input is not a letter, recursively call the function until
+    the input is valid"""
+
+    letter = input("Enter a letter : ")
+    letter = letter.lower()
+
+    if len(letter) > 1 or not letter.isalpha():
+        print("You have not entered a valid letter.")
+        return fetch_letter()
+
+    return letter
+
+
+# Hangman functions
+
+def pick_word():
+    """Function returns a random word from the list of words words_list
+
+    Uses the choice function from the random module."""
+    return choice(words_list)
+
+
+def fetch_masked_word(complete_word, found_letters):
+    """Function returns a partially or entirely masked word, based on:
+    - the original word (type str)
+    - the found letters (type list)
+
+    Returns the original word with * replacing the letters that haven't
+    been found"""
+
+    masked_word = ""
+    for letter in complete_word:
+        if letter in found_letters:
+            masked_word += letter
+        else:
+            masked_word += "*"
+
+    return masked_word
--- Python/learn-python/04_hangman/hangman.py
+++ Python/learn-python/04_hangman/hangman.py
@@ -16,3 +16,46 @@ user = fetch_username()
 # If the user doesn't have a score, add one
 if user not in scores.keys():
     scores[user] = 0
+
+# Keep track of when to stop the game
+keep_playing = 'y'
+
+while keep_playing != 'n':
+    print("Player {0}: {1} point(s)".format(user, scores[user]))
+    word_to_find = pick_word()
+    found_letters = []
+    found_word = fetch_masked_word(word_to_find, found_letters)
+    nb_chances = nb_tries
+
+    while found_word != word_to_find and nb_chances > 0:
+        print("Word to find {0} ({1} tries remaining)".format(found_word, nb_chances))
+        letter = fetch_letter()
+
+        if letter in found_letters:
+            print("You have already tried this letter.")
+        elif letter in word_to_find:
+            found_letters.append(letter)
+            print("Well done.")
+        else:
+            nb_chances -= 1
+            print("... no, this letter is not in the word...")
+
+        found_word = fetch_masked_word(word_to_find, found_letters)
+
+    # Has the word been found or all chances used?
+    if word_to_find == found_word:
+        print("Congratulations! You found the word {0}".format(word_to_find))
+    else:
+        print("HUNG !!! You lost.")
+
+    # Update the user's score
+    scores[user] += nb_chances
+
+    keep_playing = input("Do you want to keep playing (Y/N)")
+    keep_playing = keep_playing.lower()
+
+# Game is finished, save the scores
+save_scores(scores)
+
+# Display user's score
+print("You finished the game with {0} points.".format(scores[user]))
