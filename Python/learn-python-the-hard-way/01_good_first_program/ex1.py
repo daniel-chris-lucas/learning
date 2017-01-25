eeedf018156dbae9eae27e6fd77dcaac7298a835
Daniel Lucas <daniel@acid.fr>
2017-01-25 17:52:01 +0100

    Added first good program

---
+++ Python/learn-python-the-hard-way/01_good_first_program/ex1.py
@@ -0,0 +1,8 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
+print "I'd luch rather you 'not'."
+print 'I "said" do not touch this.'
+print "Another line"
