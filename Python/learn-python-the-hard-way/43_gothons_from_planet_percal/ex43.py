780df383f714113165e276217ad2489783742a53
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-02-13 00:02:11 +0100

    Engine and map

--- Python/learn-python-the-hard-way/43_gothons_from_planet_percal/ex43.py
+++ Python/learn-python-the-hard-way/43_gothons_from_planet_percal/ex43.py
@@ -1,60 +1,73 @@
+from sys import exit
+from random import randint
+
 class Scene(object):
 
     def enter(self):
-        pass
+        print "This scene is not yet configured. Subclass and implement enter()."
+        exit(1)
 
 
 class Engine(object):
 
     def __init__(self, scene_map):
-        pass
+        self.scene_map = scene_map
 
     def play(self):
-        pass
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
 
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
 
-class Death(Scene):
-
-    def enter(self):
-        pass
+        # be sure to print out the last scene
+        current_scene.enter()
 
 
 class CentralCorridor(Scene):
 
     def enter(self):
-        pass
+        print "The Gothons of Planet Percal #25 have invaded you shup and destroyed"
+        print "your entire crew. You are the last surviving member and your last"
+        print "mission is to get the neutron bomb from the Weapons Armory,"
+        print "put it in the bridge, and blow the ship up after getting into an ",
+        print "escape pod."
+        print "\n"
+        print "You're running down the central corridor to the Weapons Armory when"
+        print "a Gothon jumps out, red scaly skin, dark grimy teeth, and evil clown costume"
+        print "flowing around his hate filled body. He's blocking the door to the"
+        print "Armory and about to pull a weapon to blast you."
+
+        action = raw_input("> ")
+        
+        return 'finished'
 
 
-class LaserWeaponArmory(Scene):
+class Finished(Scene):
 
     def enter(self):
-        pass
-
-
-class TheBridge(Scene):
-
-    def enter(self):
-        pass
-
-
-class EscapePod(Scene):
-
-    def enter(self):
-        pass
+        print "You won! Good job."
+        return 'finished'
 
 
 class Map(object):
 
+    scenes = {
+        'central_corridor': CentralCorridor(),
+        'finished': Finished()
+    }
+
     def __init__(self, start_scene):
-        pass
+        self.start_scene = start_scene
 
     def next_scene(self, scene_name):
-        pass
+        return Map.scenes.get(scene_name)
 
     def opening_scene(self):
-        pass
+        return self.next_scene(self.start_scene)
 
 
-a_map = Map('central_coridor')
+a_map = Map('central_corridor')
 a_game = Engine(a_map)
 a_game.play()
