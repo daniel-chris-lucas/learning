b86a0e0f30dc144e094ac92bed4ab59ec4f59071
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-02-26 23:54:07 +0100

    Refactoring for speed

---
+++ Php/test-driven-laravel/05_refactoring_for_speed/tests/unit/ConcertTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Concert;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class ConcertTest extends TestCase
+{
+    /** @test */
+    function can_get_formatted_date()
+    {
+        $concert = factory(Concert::class)->make([
+            'date' => Carbon::parse('2016-12-01 8:00pm'),
+        ]);
+
+        $this->assertEquals('December 1, 2016', $concert->formatted_date);
+    }
+
+    /** @test */
+    function can_get_formatted_start_time()
+    {
+        $concert = factory(Concert::class)->make([
+            'date' => Carbon::parse('2016-12-01 17:00:00'),
+        ]);
+
+        $this->assertEquals('5:00pm', $concert->formatted_start_time);
+    }
+
+    /** @test */
+    function can_get_ticket_price_in_dollars()
+    {
+        $concert = factory(Concert::class)->make([
+            'ticket_price' => 6750,
+        ]);
+
+        $this->assertEquals('67.50', $concert->ticket_price_in_dollars);
+    }
+}
