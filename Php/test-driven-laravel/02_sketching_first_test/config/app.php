fb19a2463ee07cc692c5cabee2f6c2147206446e
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-02-26 22:34:25 +0100

    Moved project to Laravel 5.3

--- Php/test-driven-laravel/02_sketching_first_test/config/app.php
+++ Php/test-driven-laravel/02_sketching_first_test/config/app.php
@@ -166,7 +166,8 @@ return [
         /*
          * Package Service Providers...
          */
-        Laravel\Tinker\TinkerServiceProvider::class,
+
+        //
 
         /*
          * Application Service Providers...
@@ -196,7 +197,6 @@ return [
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
         'Blade' => Illuminate\Support\Facades\Blade::class,
-        'Broadcast' => Illuminate\Support\Facades\Broadcast::class,
         'Bus' => Illuminate\Support\Facades\Bus::class,
         'Cache' => Illuminate\Support\Facades\Cache::class,
         'Config' => Illuminate\Support\Facades\Config::class,
