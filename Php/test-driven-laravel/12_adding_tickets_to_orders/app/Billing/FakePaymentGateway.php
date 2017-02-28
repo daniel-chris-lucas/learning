f19ef1ac843410b8a28e50703d773f7fe2d08d7f
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-02-28 23:54:58 +0100

    Adding tickets to orders

---
+++ Php/test-driven-laravel/12_adding_tickets_to_orders/app/Billing/FakePaymentGateway.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Billing;
+
+use App\Billing\PaymentGateway;
+
+class FakePaymentGateway implements PaymentGateway
+{
+    private $charges;
+
+    public function __construct()
+    {
+        $this->charges = collect();
+    }
+
+    public function getValidTestToken()
+    {
+        return "valid-token";
+    }
+
+    public function charge($amount, $token)
+    {
+        $this->charges[] = $amount;
+    }
+
+    public function totalCharges()
+    {
+        return $this->charges->sum();
+    }
+}
