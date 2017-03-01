904a9153a6be914ad8739a826bdbce209b304d1c
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-03-01 23:03:50 +0100

    Adding tickets to concerts

---
+++ Php/test-driven-laravel/19_adding_tickets_to_concerts/app/Concert.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Concert extends Model
+{
+    protected $guarded = [];
+    protected $dates = ['date'];
+
+    public function scopePublished($query)
+    {
+        return $query->whereNotNull('published_at');
+    }
+
+    public function getFormattedDateAttribute()
+    {
+        return $this->date->format('F j, Y');
+    }
+
+    public function getFormattedStartTimeAttribute()
+    {
+        return $this->date->format('g:ia');
+    }
+
+    public function getTicketPriceInDollarsAttribute()
+    {
+        return number_format($this->ticket_price / 100, 2);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
+    public function orderTickets($email, $ticketQuantity)
+    {
+        $order = $this->orders()->create(['email' => $email]);
+        $tickets = $this->tickets()->take($ticketQuantity)->get();
+
+        foreach ($tickets as $ticket) {
+            $order->tickets()->save($ticket);
+        }
+
+        return $order;
+    }
+
+    public function addTickets($quantity)
+    {
+        foreach (range(1, $quantity) as $i) {
+            $this->tickets()->create([]);
+        }
+    }
+
+    public function ticketsRemaining()
+    {
+        return $this->tickets()->whereNull('order_id')->count();
+    }
+}
