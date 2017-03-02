0c035b555c32027ebe742b3db81f9b7df969bfb7
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-03-02 21:06:09 +0100

    Cancelling failed orders

---
+++ Php/test-driven-laravel/22_cancelling_failed_orders/app/Order.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $guarded = [];
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
+    public function cancel()
+    {
+        foreach ($this->tickets as $ticket) {
+            $ticket->update(['order_id' => null]);
+        }
+
+        $this->delete();
+    }
+}
---
+++ Php/test-driven-laravel/22_cancelling_failed_orders/app/Ticket.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    protected $guarded = [];
+    
+    public function scopeAvailable($query)
+    {
+        return $query->whereNull('order_id');
+    }
+}
