4674c3dc09d4cfb969d71cc60a4c0646dee0f2f9
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-03-02 20:34:45 +0100

    Finishing the feature test

---
+++ Php/test-driven-laravel/21_finishing_the_feature_test/app/Http/Controllers/ConcertOrdersController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Billing\PaymentFailedException;
+use App\Billing\PaymentGateway;
+use App\Concert;
+use App\Exceptions\NotEnoughTicketsException;
+use Illuminate\Http\Request;
+
+class ConcertOrdersController extends Controller
+{
+    private $paymentGateway;
+
+    public function __construct(PaymentGateway $paymentGateway)
+    {
+        $this->paymentGateway = $paymentGateway;
+    }
+
+    public function store($concertId)
+    {
+        $concert = Concert::published()->findOrFail($concertId);
+        
+        $this->validate(request(), [
+            'email' => ['required', 'email'],
+            'ticket_quantity' => ['required', 'integer', 'min:1'],
+            'payment_token' => ['required']
+        ]);
+        
+        try {
+            $order = $concert->orderTickets(request('email'), request('ticket_quantity'));
+            $this->paymentGateway->charge(request('ticket_quantity') * $concert->ticket_price, request('payment_token'));
+
+            return response()->json([], 201);
+        } catch (PaymentFailedException $e) {
+            return response()->json([], 422);
+        } catch (NotEnoughTicketsException $e) {
+            return response()->json([], 422);
+        }
+    }
+}
---
+++ Php/test-driven-laravel/21_finishing_the_feature_test/tests/unit/ConcertTest.php
@@ -0,0 +1,127 @@
+<?php
+
+use App\Concert;
+use App\Exceptions\NotEnoughTicketsException;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class ConcertTest extends TestCase
+{
+    use DatabaseMigrations;
+    
+    /** @test */
+    function can_get_formatted_date()
+    {
+        $concert = factory(Concert::class)->make([
+            'date' => Carbon::parse('2016-12-01 8:00pm'),
+        ]);
+
+        $this->assertEquals('December 1, 2016', $concert->formatted_date);
+    }
+
+    /** @test */
+    function can_get_formatted_start_time()
+    {
+        $concert = factory(Concert::class)->make([
+            'date' => Carbon::parse('2016-12-01 17:00:00'),
+        ]);
+
+        $this->assertEquals('5:00pm', $concert->formatted_start_time);
+    }
+
+    /** @test */
+    function can_get_ticket_price_in_dollars()
+    {
+        $concert = factory(Concert::class)->make([
+            'ticket_price' => 6750,
+        ]);
+
+        $this->assertEquals('67.50', $concert->ticket_price_in_dollars);
+    }
+
+    /** @test */
+    function concerts_with_a_published_at_date_are_published()
+    {
+        $publishedConcertA = factory(Concert::class)->create(['published_at' => Carbon::parse('-1 week')]);
+        $publishedConcertB = factory(Concert::class)->create(['published_at' => Carbon::parse('-1 week')]);
+        $unpublishedConcert = factory(Concert::class)->create(['published_at' => null]);
+
+        $publishedConcerts = Concert::published()->get();
+
+        $this->assertTrue($publishedConcerts->contains($publishedConcertA));
+        $this->assertTrue($publishedConcerts->contains($publishedConcertB));
+        $this->assertFalse($publishedConcerts->contains($unpublishedConcert));
+    }
+
+    /** @test */
+    function can_order_concert_tickets()
+    {
+        $concert = factory(Concert::class)->create();
+        $concert->addTickets(3);
+
+        $order = $concert->orderTickets('jane@example.com', 3);
+
+        $this->assertEquals('jane@example.com', $order->email);
+        $this->assertEquals(3, $order->tickets()->count());
+    }
+
+    /** @test */
+    function can_add_tickets()
+    {
+        $concert = factory(Concert::class)->create();
+
+        $concert->addTickets(50);
+
+        $this->assertEquals(50, $concert->ticketsRemaining());
+    }
+
+    /** @test */
+    function tickets_remaining_does_not_include_tickets_associated_with_an_order()
+    {
+        $concert = factory(Concert::class)->create();
+        $concert->addTickets(50);
+        $concert->orderTickets('jane@example.com', 30);
+
+        $this->assertEquals(20, $concert->ticketsRemaining());
+    }
+
+    /** @test */
+    function trying_to_purchase_more_tickets_than_remain_throws_an_exception()
+    {
+        $concert = factory(Concert::class)->create();
+        $concert->addTickets(10);
+
+        try {
+            $concert->orderTickets('jane@example.com', 11);
+        } catch (NotEnoughTicketsException $e) {
+            $order = $concert->orders()->where('email', 'jane@example.com')->first();
+            $this->assertNull($order);
+            $this->assertEquals(10, $concert->ticketsRemaining());
+            return;
+        }
+
+        $this->fail("Order succeeded even though there were not enough tickets remaining");
+    }
+
+    /** @test */
+    function cannot_order_tickets_that_have_already_been_purchased()
+    {
+        $concert = factory(Concert::class)->create();
+        $concert->addTickets(10);
+
+        $concert->orderTickets('jane@example.com', 8);
+
+        try {
+            $concert->orderTickets('john@example.com', 3);
+        } catch (NotEnoughTicketsException $e) {
+            $johnsOrder = $concert->orders()->where('email', 'john@example.com')->first();
+            $this->assertNull($johnsOrder);
+            $this->assertEquals(2, $concert->ticketsRemaining());
+            return;
+        }
+
+        $this->fail("Order succeeded even though there were not enough tickets remaining");
+    }
+}
