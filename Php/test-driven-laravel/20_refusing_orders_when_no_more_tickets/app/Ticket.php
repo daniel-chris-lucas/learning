bbf61c53d5e658fc03090fe3349df1a93fb1ebfb
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-03-02 20:20:05 +0100

    Refusing orders where there are no more tickets

---
+++ Php/test-driven-laravel/20_refusing_orders_when_no_more_tickets/app/Ticket.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    public function scopeAvailable($query)
+    {
+        return $query->whereNull('order_id');        
+    }
+}
