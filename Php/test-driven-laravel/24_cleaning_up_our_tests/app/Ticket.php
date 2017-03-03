6cd3229d441da4fb2b06c3aaf952c289a628362c
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-03-03 22:01:34 +0100

    Cleaning up our tests

---
+++ Php/test-driven-laravel/24_cleaning_up_our_tests/app/Ticket.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    protected $guarded = [];
+    
+    public function scopeAvailable($query)
+    {
+        return $query->whereNull('order_id');
+    }
+
+    public function release()
+    {
+        $this->update(['order_id' => null]);
+    }
+}
---
+++ Php/test-driven-laravel/24_cleaning_up_our_tests/tests/unit/OrderTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Concert;
+use App\Order;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class OrderTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function tickets_are_released_when_an_order_is_cancelled()
+    {
+        $concert = factory(Concert::class)->create()->addTickets(10);
+        $order = $concert->orderTickets('jane@example.com', 5);
+        $this->assertEquals(5, $concert->ticketsRemaining());
+
+        $order->cancel();
+
+        $this->assertEquals(10, $concert->ticketsRemaining());
+        $this->assertNull(Order::find($order->id));
+    }
+}
