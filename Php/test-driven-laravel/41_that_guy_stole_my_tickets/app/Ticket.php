5bc0e3966bb9251f1bfafabe91607d52570934a3
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-03-06 14:32:07 +0100

    That guy stole my tickets!

---
+++ Php/test-driven-laravel/41_that_guy_stole_my_tickets/app/Ticket.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    protected $guarded = [];
+    
+    public function scopeAvailable($query)
+    {
+        return $query->whereNull('order_id')->whereNull('reserved_at');
+    }
+
+    public function reserve()
+    {
+        $this->update(['reserved_at' => Carbon::now()]);
+    }
+
+    public function release()
+    {
+        $this->update(['order_id' => null]);
+    }
+
+    public function concert()
+    {
+        return $this->belongsTo(Concert::class);
+    }
+
+    public function getPriceAttribute()
+    {
+        return $this->concert->ticket_price;
+    }
+}
