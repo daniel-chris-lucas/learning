787362d8ddc39642c869a5ea04dc282280fed7da
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-03-06 13:20:39 +0100

    Requestception

---
+++ Php/test-driven-laravel/34_uh_oh_a_race_condition/tests/unit/ReservationTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Concert;
+use App\Reservation;
+use App\Ticket;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class ReservationTest extends TestCase
+{
+    /** @test */
+    function calculating_the_total_cost()
+    {
+        $tickets = collect([
+            (object) ['price' => 1200],
+            (object) ['price' => 1200],
+            (object) ['price' => 1200],
+        ]);
+
+        $reservation = new Reservation($tickets);
+
+        $this->assertEquals(3600, $reservation->totalCost());
+    }
+}
