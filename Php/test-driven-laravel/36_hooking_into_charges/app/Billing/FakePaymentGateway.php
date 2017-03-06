1dc7242b383a3266c0f9e396e520be69eae05087
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-03-06 13:32:20 +0100

    Hooking into charges

---
+++ Php/test-driven-laravel/36_hooking_into_charges/app/Billing/FakePaymentGateway.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Billing;
+
+class FakePaymentGateway implements PaymentGateway
+{
+    private $charges;
+    private $beforeFirstChargeCallback;
+
+    public function __construct()
+    {
+        $this->charges = collect();
+    }
+
+    public function getValidTestToken()
+    {
+        return "valid-token";
+    }
+
+    public function charge($amount, $token)
+    {
+        if ($this->beforeFirstChargeCallback !== null) {
+            $this->beforeFirstChargeCallback->__invoke($this);
+        }
+
+        if ($token !== $this->getValidTestToken()) {
+            throw new PaymentFailedException;
+        }
+        $this->charges[] = $amount;
+    }
+
+    public function totalCharges()
+    {
+        return $this->charges->sum();
+    }
+
+    public function beforeFirstCharge($callback)
+    {
+        $this->beforeFirstChargeCallback = $callback;
+    }
+}
