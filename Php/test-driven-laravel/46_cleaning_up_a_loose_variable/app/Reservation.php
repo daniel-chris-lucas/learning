25ab532eb0233bac0f93ef1661b9c3bc492d73b7
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-03-06 17:51:46 +0100

    Cleaning up a loose variable

---
+++ Php/test-driven-laravel/46_cleaning_up_a_loose_variable/app/Reservation.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+class Reservation
+{
+    private $tickets;
+
+    public function __construct($tickets)
+    {
+        $this->tickets = $tickets;
+    }
+
+    public function totalCost()
+    {
+        return $this->tickets->sum('price');
+    }
+
+    public function tickets()
+    {
+        return $this->tickets;
+    }
+
+    public function cancel()
+    {
+        foreach ($this->tickets as $ticket) {
+            $ticket->release();
+        }
+    }
+}
