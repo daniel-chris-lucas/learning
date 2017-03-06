99ea7f73ee9e5260c341266b508b6b49600cba23
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-03-06 17:22:27 +0100

    Refactoring mocks to spies

---
+++ Php/test-driven-laravel/43_refactoring_mocks_to_spies/tests/unit/TicketTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Concert;
+use App\Ticket;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class TicketTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function a_ticket_can_be_reserved()
+    {
+        $ticket = factory(Ticket::class)->create();
+        $this->assertNull($ticket->reserved_at);
+
+        $ticket->reserve();
+
+        $this->assertNotNull($ticket->fresh()->reserved_at);
+    }
+
+    /** @test */
+    function a_ticket_can_be_released()
+    {
+        $concert = factory(Concert::class)->create();
+        $concert->addTickets(1);
+        $order = $concert->orderTickets('jane@example.com', 1);
+        $ticket = $order->tickets()->first();
+        $this->assertEquals($order->id, $ticket->order_id);
+
+        $ticket->release();
+
+        $this->assertNull($ticket->fresh()->order_id);
+    }
+}
