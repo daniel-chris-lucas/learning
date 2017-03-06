d139499f646083cc0d7e81b685b0ac5ae403725b
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-03-06 17:11:40 +0100

    Cancelling reservations

---
+++ Php/test-driven-laravel/42_cancelling_reservations/tests/unit/ReservationTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Concert;
+use App\Reservation;
+use App\Ticket;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class ReservationTest extends TestCase
+{
+    /** @test */
+    function calculating_the_total_cost()
+    {
+        $tickets = collect([
+            (object) ['price' => 1200],
+            (object) ['price' => 1200],
+            (object) ['price' => 1200],
+        ]);
+
+        $reservation = new Reservation($tickets);
+
+        $this->assertEquals(3600, $reservation->totalCost());
+    }
+
+    /** @test */
+    function reserved_tickets_are_released_when_a_reservation_is_cancelled()
+    {
+        $ticket1 = Mockery::mock(Ticket::class);
+        $ticket1->shouldReceive('release')->once();
+
+        $ticket2 = Mockery::mock(Ticket::class);
+        $ticket2->shouldReceive('release')->once();
+
+        $ticket3 = Mockery::mock(Ticket::class);
+        $ticket3->shouldReceive('release')->once();
+
+        $tickets = collect([$ticket1, $ticket2, $ticket3]);
+        $reservation = new Reservation($tickets);
+
+        $reservation->cancel();
+    }
+}
