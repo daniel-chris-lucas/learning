9be52ba155728574a9ac3448a10164e4b055f058
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-03-06 12:24:49 +0100

    Precomputing the order amount

---
+++ Php/test-driven-laravel/31_precomputing_the_order_amount/database/migrations/2017_02_28_225017_create_tickets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('concert_id');
+            $table->unsignedInteger('order_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
