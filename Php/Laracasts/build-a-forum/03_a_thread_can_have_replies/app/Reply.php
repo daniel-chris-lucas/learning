842d6d7c616099fe659fe52a61421a599ff8858b
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-04-23 01:35:21 +0200

    A thread can have replies

---
+++ Php/Laracasts/build-a-forum/03_a_thread_can_have_replies/app/Reply.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reply extends Model
+{
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
---
+++ Php/Laracasts/build-a-forum/03_a_thread_can_have_replies/resources/views/threads/show.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">{{ $thread->title }}</div>
+
+                    <div class="panel-body">
+                        {{ $thread->body }}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                @foreach ($thread->replies as $reply)
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <a href="#">{{ $reply->owner->name }}</a>
+                            said {{ $reply->created_at->diffForHumans() }}
+                        </div>
+                        <div class="panel-body">
+                            {{ $reply->body }}
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+@endsection
