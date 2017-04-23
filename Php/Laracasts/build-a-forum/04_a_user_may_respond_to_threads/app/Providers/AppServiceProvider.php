559dae15f5641cd15f601b1481191b93f148f8b9
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-04-23 23:33:05 +0200

    A user may respond to threads. #WIP

---
+++ Php/Laracasts/build-a-forum/04_a_user_may_respond_to_threads/app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Fix for utf8mb4 on Mariadb
+        // See https://laravel-news.com/laravel-5-4-key-too-long-error
+        Schema::defaultStringLength(191);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ Php/Laracasts/build-a-forum/04_a_user_may_respond_to_threads/resources/views/threads/show.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        {{ $thread->title }}
+                    </div>
+
+                    <div class="panel-body">
+                        {{ $thread->body }}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                @foreach ($thread->replies as $reply)
+                    @include('threads.reply')
+                @endforeach
+            </div>
+        </div>
+    </div>
+@endsection
