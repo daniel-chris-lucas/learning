12891f020a5f0da4e7a4dd90ae14ab5d3e6d1653
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-04-30 22:39:57 +0200

    How to test validation errors

---
+++ Php/Laracasts/build-a-forum/10_testing_validation_errors/app/Http/Controllers/ThreadsController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Thread;
+use Illuminate\Http\Request;
+
+class ThreadsController extends Controller
+{
+    /**
+     * ThreadsController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $threads = Thread::latest()->get();
+
+        return view('threads.index', compact('threads'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('threads.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title'      => 'required',
+            'body'       => 'required',
+            'channel_id' => 'required|exists:channels,id',
+        ]);
+
+        $thread = Thread::create([
+            'user_id'    => auth()->id(),
+            'channel_id' => request('channel_id'),
+            'title'      => request('title'),
+            'body'       => request('body'),
+        ]);
+
+        return redirect($thread->path());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param              $channelId
+     * @param  \App\Thread $thread
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($channelId, Thread $thread)
+    {
+        return view('threads.show', compact('thread'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Thread $thread
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Thread $thread)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Thread              $thread
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Thread $thread)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Thread $thread
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Thread $thread)
+    {
+        //
+    }
+}
---
+++ Php/Laracasts/build-a-forum/10_testing_validation_errors/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+Route::get('/threads', 'ThreadsController@index');
+Route::get('/threads/create', 'ThreadsController@create');
+Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
+Route::post('/threads', 'ThreadsController@store');
+Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
