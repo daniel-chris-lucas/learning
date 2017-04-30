c1f045f588bc2d42fd412fbbeea82f21225edf9b
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-04-30 17:21:04 +0200

    The exception handling conundrum

---
+++ Php/Laracasts/build-a-forum/07_the_exception_handling_conundrum/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+// Route::get('/threads', 'ThreadsController@index');
+// Route::get('/threads/create', 'ThreadsController@create');
+// Route::post('/threads', 'ThreadsController@store');
+// Route::get('/threads/{thread}', 'ThreadsController@show');
+Route::resource('threads', 'ThreadsController');
+Route::post('/threads/{thread}/replies', 'RepliesController@store');
---
+++ Php/Laracasts/build-a-forum/07_the_exception_handling_conundrum/tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Thread;
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CreateThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function guests_may_not_create_threads()
+    {
+        $this->expectException(\Illuminate\Auth\AuthenticationException::class);
+        
+        $thread = make(Thread::class);
+
+        $this->post('/threads', $thread->toArray());
+    }
+
+    /** @test */
+    function guests_cannot_see_the_create_thread_page()
+    {
+        $this->withExceptionHandling()
+            ->get('/threads/create')
+            ->assertRedirect('/login');
+    }
+
+    /** @test */
+    function an_authenticated_user_can_create_new_forum_threads()
+    {
+        $this->signIn();
+
+        $thread = make(Thread::class);
+
+        $this->post('/threads', $thread->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+}
