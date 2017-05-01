610ae4892017cd46a058088eb515e79431fc2af8
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-05-01 23:24:39 +0200

    A lesson in refactoring

---
+++ Php/Laracasts/build-a-forum/15_a_lesson_in_refactoring/app/Filters/ThreadFilters.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filters;
+
+use App\User;
+
+class ThreadFilters extends Filters
+{
+    protected $filters = ['by'];
+
+    /**
+     * Filter the query by a given username.
+     *
+     * @param string $username
+     *
+     * @return mixed
+     */
+    protected function by($username)
+    {
+        $user = User::where('name', $username)->firstOrFail();
+
+        return $this->builder->where('user_id', $user->id);
+    }
+}
---
+++ Php/Laracasts/build-a-forum/15_a_lesson_in_refactoring/app/Thread.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Thread extends Model
+{
+    protected $guarded = [];
+    
+    public function path()
+    {
+        return "/threads/{$this->channel->slug}/{$this->id}";
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    
+    public function channel()
+    {
+        return $this->belongsTo(Channel::class);
+    }
+
+    public function addReply($reply)
+    {
+        $this->replies()->create($reply);
+    }
+
+    public function scopeFilter($query, $filters)
+    {
+        return $filters->apply($query);
+    }
+}
