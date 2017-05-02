fa34623da82b2d5da7a3a4c739a19aaf3f520c3c
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-05-03 00:15:25 +0200

    How to construct an activity feed with TDD

---
+++ Php/Laracasts/build-a-forum/25_construct_activity_feed_with_tdd/app/Reply.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reply extends Model
+{
+    use Favoritable, RecordsActivity;
+
+    protected $guarded = [];
+
+    protected $with = ['owner', 'favorites'];
+    
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
---
+++ Php/Laracasts/build-a-forum/25_construct_activity_feed_with_tdd/tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Channel;
+use App\Reply;
+use App\Thread;
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ReadThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = create(Thread::class);
+    }
+
+    /** @test */
+    public function a_user_can_view_all_threads()
+    {
+        $this->get('/threads')
+            ->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_read_a_single_thread()
+    {
+        $this->get($this->thread->path())
+            ->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        $reply = create(Reply::class, ['thread_id' => $this->thread->id]);
+
+        $this->get($this->thread->path())
+            ->assertSee($reply->body);
+    }
+
+    /** @test */
+    function a_user_can_filter_threads_according_to_a_channel()
+    {
+        $channel = create(Channel::class);
+        $threadInChannel = create(Thread::class, ['channel_id' => $channel->id]);
+        $threadNotInChannel = create(Thread::class);
+
+        $this->get('/threads/' . $channel->slug)
+            ->assertSee($threadInChannel->title)
+            ->assertDontSee($threadNotInChannel->title);
+    }
+
+    /** @test */
+    function a_user_can_filter_threads_by_any_username()
+    {
+        $this->signIn(create(User::class, ['name' => 'JohnDoe']));
+
+        $threadByJohn = create(Thread::class, ['user_id' => auth()->id()]);
+        $threadNotByJohn = create(Thread::class);
+
+        $this->get('/threads?by=JohnDoe')
+            ->assertSee($threadByJohn->title)
+            ->assertDontSee($threadNotByJohn->title);
+    }
+
+    /** @test */
+    function a_user_can_filter_threads_by_popularity()
+    {
+        $threadWithTwoReplies = create(Thread::class);
+        create(Reply::class, ['thread_id' => $threadWithTwoReplies->id], 2);
+
+        $threadWithThreeReplies = create(Thread::class);
+        create(Reply::class, ['thread_id' => $threadWithThreeReplies->id], 3);
+
+        $threadWithNoReplies = $this->thread;
+
+        $response = $this->getJson('/threads?popular=1')->json();
+
+        $this->assertEquals([3, 2, 0], array_column($response, 'replies_count'));
+    }
+}
