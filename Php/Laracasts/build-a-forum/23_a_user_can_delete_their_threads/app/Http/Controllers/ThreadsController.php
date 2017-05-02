e6f1536ef5502ec49dd1510aeb3022722d4833af
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-05-02 19:28:15 +0200

    A user can delete their threads

---
+++ Php/Laracasts/build-a-forum/23_a_user_can_delete_their_threads/app/Http/Controllers/ThreadsController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Channel;
+use App\Thread;
+use App\Filters\ThreadFilters;
+use App\User;
+use Illuminate\Http\Request;
+
+class ThreadsController extends Controller
+{
+    /**
+     * ThreadsController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Channel       $channel
+     *
+     * @param ThreadFilters $filters
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Channel $channel, ThreadFilters $filters)
+    {
+        $threads = $this->getThreads($channel, $filters);
+
+        if (request()->wantsJson()) {
+            return $threads;
+        }
+
+        return view('threads.index', compact('threads'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('threads.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title'      => 'required',
+            'body'       => 'required',
+            'channel_id' => 'required|exists:channels,id',
+        ]);
+
+        $thread = Thread::create([
+            'user_id'    => auth()->id(),
+            'channel_id' => request('channel_id'),
+            'title'      => request('title'),
+            'body'       => request('body'),
+        ]);
+
+        return redirect($thread->path());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param              $channel
+     * @param  \App\Thread $thread
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($channel, Thread $thread)
+    {
+        return view('threads.show', [
+            'thread'  => $thread,
+            'replies' => $thread->replies()->paginate(20),
+        ]);
+    }
+
+    public function destroy($channel, Thread $thread)
+    {
+        $thread->delete();
+
+        if (request()->wantsJson()) {
+            return response([], 204);
+        }
+
+        return redirect('/threads');
+    }
+
+    /**
+     * @param Channel       $channel
+     * @param ThreadFilters $filters
+     *
+     * @return mixed
+     */
+    protected function getThreads(Channel $channel, ThreadFilters $filters)
+    {
+        $threads = Thread::latest()->filter($filters);
+
+        if ($channel->exists) {
+            $threads->where('channel_id', $channel->id);
+        }
+
+        $threads = $threads->get();
+
+        return $threads;
+    }
+}
