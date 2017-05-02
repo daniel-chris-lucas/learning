fd1d13ec4ef7ef66fe7d856da214aefb4561b1b6
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-05-02 13:09:41 +0200

    The favorite button

---
+++ Php/Laracasts/build-a-forum/19_the_favorite_button/app/Reply.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reply extends Model
+{
+    protected $guarded = [];
+    
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function favorites()
+    {
+         return $this->morphMany(Favorite::class, 'favorited');
+    }
+
+    public function favorite()
+    {
+        $attributes = ['user_id' => auth()->id()];
+
+        if (! $this->favorites()->where($attributes)->exists()) {
+            return $this->favorites()->create($attributes);
+        }
+    }
+
+    public function isFavorited()
+    {
+        return $this->favorites()->where('user_id', auth()->id())->exists();
+    }
+}
