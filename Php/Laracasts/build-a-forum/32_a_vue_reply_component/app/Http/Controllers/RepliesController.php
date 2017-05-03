0ef0a9f25b812230252646282be1850352d16d4b
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-05-03 15:32:02 +0200

    A Vue reply component

---
+++ Php/Laracasts/build-a-forum/32_a_vue_reply_component/app/Http/Controllers/RepliesController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Reply;
+use App\Thread;
+use Illuminate\Http\Request;
+
+class RepliesController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Persist a new reply.
+     *
+     * @param        $channelId
+     * @param Thread $thread
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store($channelId, Thread $thread)
+    {
+        $this->validate(request(), ['body' => 'required']);
+
+        $thread->addReply([
+            'body' => request('body'),
+            'user_id' => auth()->id()
+        ]);
+
+        return back()->with('flash', 'Your reply has been left.');
+    }
+
+    public function update(Reply $reply)
+    {
+        $this->authorize('update', $reply);
+
+        $reply->update(request(['body']));
+    }
+
+    public function destroy(Reply $reply)
+    {
+        $this->authorize('update', $reply);
+        
+        $reply->delete();
+
+        return back();
+    }
+}
