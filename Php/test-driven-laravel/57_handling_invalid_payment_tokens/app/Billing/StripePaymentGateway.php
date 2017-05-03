ec60dd4595de4f58fa8a8eddfcc3889312993acd
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-05-04 00:06:05 +0200

    Handling invalid payment tokens

---
+++ Php/test-driven-laravel/57_handling_invalid_payment_tokens/app/Billing/StripePaymentGateway.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Billing;
+
+use App\Billing\PaymentFailedException;
+use Stripe\Charge;
+use Stripe\Error\InvalidRequest;
+
+class StripePaymentGateway implements PaymentGateway
+{
+    private $apiKey;
+
+    public function __construct($apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    public function charge($amount, $token)
+    {
+        try {
+            Charge::create([
+                'amount' => $amount,
+                'source' => $token,
+                'currency' => 'usd',
+            ], ['api_key' => $this->apiKey]);
+        } catch (InvalidRequest $e) {
+            throw new PaymentFailedException;
+        }
+    }
+}
