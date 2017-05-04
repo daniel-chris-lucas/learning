3ee0446cc22615476c010e36abaa0f340264a837
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-05-04 18:03:05 +0200

    Capturing charges with callbacks

---
+++ Php/test-driven-laravel/60_capturing_charges_with_callbacks/tests/unit/Billing/FakePaymentGatewayTest.php
@@ -0,0 +1,55 @@
+<?php
+
+use App\Billing\FakePaymentGateway;
+use App\Billing\PaymentFailedException;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class FakePaymentGatewayTest extends TestCase
+{
+    protected function getPaymentGateway()
+    {
+        return new FakePaymentGateway;
+    }
+
+    /** @test */
+    function charges_with_a_valid_payment_token_are_successful()
+    {
+        $paymentGateway = $this->getPaymentGateway();
+
+        $paymentGateway->charge(2500, $paymentGateway->getValidTestToken());
+
+        $this->assertEquals(2500, $paymentGateway->totalCharges());
+    }
+
+    /** @test */
+    function charges_with_an_invalid_payment_token_fail()
+    {
+        try {
+            $paymentGateway = new FakePaymentGateway;
+            $paymentGateway->charge(2500, 'invalid-payment-token');
+        } catch (PaymentFailedException $e) {
+            return;
+        }
+
+        $this->fail();
+    }
+
+    /** @test */
+    function running_a_hook_before_the_first_charge()
+    {
+        $paymentGateway = new FakePaymentGateway;
+        $timesCallbackRan = 0;
+
+        $paymentGateway->beforeFirstCharge(function ($paymentGateway) use (&$timesCallbackRan) {
+            $timesCallbackRan++;
+            $paymentGateway->charge(2500, $paymentGateway->getValidTestToken());
+            $this->assertEquals(2500, $paymentGateway->totalCharges());
+        });
+
+        $paymentGateway->charge(2500, $paymentGateway->getValidTestToken());
+        $this->assertEquals(1, $timesCallbackRan);
+        $this->assertEquals(5000, $paymentGateway->totalCharges());
+    }
+}
