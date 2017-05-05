174c3b66c976648abbb1de811ec0d4f18bdd02f6
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-05-05 14:07:18 +0200

    Driving out the endpoint

---
+++ Php/test-driven-laravel/67_driving_out_the_endpoint/tests/unit/TicketTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Concert;
+use App\Ticket;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class TicketTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function a_ticket_can_be_reserved()
+    {
+        $ticket = factory(Ticket::class)->create();
+        $this->assertNull($ticket->reserved_at);
+
+        $ticket->reserve();
+
+        $this->assertNotNull($ticket->fresh()->reserved_at);
+    }
+
+    /** @test */
+    function a_ticket_can_be_released()
+    {
+        $ticket = factory(Ticket::class)->states('reserved')->create();
+        $this->assertNotNull($ticket->reserved_at);
+
+        $ticket->release();
+
+        $this->assertNull($ticket->fresh()->reserved_at);
+    }
+}
