b9d45cdc1d765c060aa79ac8a433a668a9db13eb
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-05-05 17:27:41 +0200

    Promoting charges to objects

---
+++ Php/test-driven-laravel/79_promoting_charges_to_objects/app/Providers/AppServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use App\Billing\PaymentGateway;
+use App\Billing\StripePaymentGateway;
+use App\OrderConfirmationNumberGenerator;
+use App\RandomOrderConfirmationNumberGenerator;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(StripePaymentGateway::class, function () {
+            return new StripePaymentGateway(config('services.stripe.secret'));
+        });
+
+        $this->app->bind(PaymentGateway::class, StripePaymentGateway::class);
+        $this->app->bind(OrderConfirmationNumberGenerator::class, RandomOrderConfirmationNumberGenerator::class);
+    }
+}
