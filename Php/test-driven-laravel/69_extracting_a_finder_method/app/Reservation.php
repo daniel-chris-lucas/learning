b07dc7701c3f0750fbfa92f0c6a460c7f54630d7
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-05-05 14:51:57 +0200

    Extracting a finder method

---
+++ Php/test-driven-laravel/69_extracting_a_finder_method/app/Reservation.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+use App\Order;
+
+class Reservation
+{
+    private $tickets;
+    private $email;
+
+    public function __construct($tickets, $email)
+    {
+        $this->tickets = $tickets;
+        $this->email = $email;
+    }
+
+    public function totalCost()
+    {
+        return $this->tickets->sum('price');
+    }
+
+    public function tickets()
+    {
+        return $this->tickets;
+    }
+
+    public function email()
+    {
+        return $this->email;
+    }
+
+    public function complete($paymentGateway, $paymentToken)
+    {
+        $paymentGateway->charge($this->totalCost(), $paymentToken);
+        
+        return Order::forTickets($this->tickets(), $this->email(), $this->totalCost());
+    }
+
+    public function cancel()
+    {
+        foreach ($this->tickets as $ticket) {
+            $ticket->release();
+        }
+    }
+}
---
+++ Php/test-driven-laravel/69_extracting_a_finder_method/tests/features/ViewOrderTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Concert;
+use App\Order;
+use App\Ticket;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class ExampleTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function user_can_view_their_order_confirmation()
+    {
+        $this->disableExceptionHandling();
+        
+        // Create a concert
+        $concert = factory(Concert::class)->create();
+        // Create an order
+        $order = factory(Order::class)->create([
+            'confirmation_number' => 'ORDERCONFIRMATION1234'
+        ]);
+        // Create a ticket
+        $ticket = factory(Ticket::class)->create([
+            'concert_id' => $concert->id,
+            'order_id' => $order->id,
+        ]);
+        
+        // Visit the order confirmation page
+        $response = $this->get("/orders/ORDERCONFIRMATION1234");
+
+        $response->assertStatus(200);
+        
+        // Assert we see the correct order details
+        $response->assertViewHas('order', function ($viewOrder) use ($order) {
+            return $order->id === $viewOrder->id;
+        });
+    }
+}
