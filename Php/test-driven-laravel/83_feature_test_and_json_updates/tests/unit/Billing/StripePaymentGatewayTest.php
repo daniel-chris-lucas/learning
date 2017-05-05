0dfa8e85b78a97ea63a8775ee79ab6dd1a4f3d36
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-05-05 19:36:43 +0200

    Feature test and JSON updates

---
+++ Php/test-driven-laravel/83_feature_test_and_json_updates/tests/unit/Billing/StripePaymentGatewayTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Billing\PaymentFailedException;
+use App\Billing\StripePaymentGateway;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+/**
+ * @group integration
+ */
+class StripePaymentGatewayTest extends TestCase
+{
+    use PaymentGatewayContractTests;
+
+    protected function getPaymentGateway()
+    {
+        return new StripePaymentGateway(config('services.stripe.secret'));
+    }
+}
---
+++ Php/test-driven-laravel/83_feature_test_and_json_updates/tests/unit/OrderTest.php
@@ -0,0 +1,83 @@
+<?php
+
+use App\Billing\Charge;
+use App\Concert;
+use App\Order;
+use App\Reservation;
+use App\Ticket;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class OrderTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function creating_an_order_from_tickets_email_and_charge()
+    {
+        $tickets = factory(Ticket::class, 3)->create();
+
+        $charge = new Charge(['amount' => 3600, 'card_last_four' => '1234']);
+
+        $order = Order::forTickets($tickets, 'john@example.com', $charge);
+
+        $this->assertEquals('john@example.com', $order->email);
+        $this->assertEquals(3, $order->ticketQuantity());
+        $this->assertEquals(3600, $order->amount);
+        $this->assertEquals('1234', $order->card_last_four);
+    }
+
+    /** @test */
+    function retrieving_an_order_by_confirmation_number()
+    {
+        $order = factory(Order::class)->create([
+            'confirmation_number' => 'ORDERCONFIRMATION1234',
+        ]);
+
+        $foundOrder = Order::findByConfirmationNumber('ORDERCONFIRMATION1234');
+
+        $this->assertEquals($order->id, $foundOrder->id);
+    }
+
+    /** @test */
+    function retrieving_a_nonexistent_order_by_confirmation_number_throws_an_exception()
+    {
+        try {
+            Order::findByConfirmationNumber('NONEXISTENTCONFIRMATIONNUMBER');
+        } catch (ModelNotFoundException $e) {
+            return;
+        }
+
+        $this->fail('No matching order was found for the specified confirmation number, but an exception was not thrown.');
+    }
+
+    /** @test */
+    function converting_to_an_array()
+    {
+        $order = factory(Order::class)->create([
+            'confirmation_number' => 'ORDERCONFIRMATION1234',
+            'email' => 'jane@example.com',
+            'amount' => 6000,
+        ]);
+        $order->tickets()->saveMany([
+            factory(Ticket::class)->create(['code' => 'TICKETCODE1']),
+            factory(Ticket::class)->create(['code' => 'TICKETCODE2']),
+            factory(Ticket::class)->create(['code' => 'TICKETCODE3']),
+        ]);
+
+        $result = $order->toArray();
+
+        $this->assertEquals([
+            'confirmation_number' => 'ORDERCONFIRMATION1234',
+            'email' => 'jane@example.com',
+            'amount' => 6000,
+            'tickets' => [
+                ['code' => 'TICKETCODE1'],
+                ['code' => 'TICKETCODE2'],
+                ['code' => 'TICKETCODE3'],
+            ]
+        ], $result);
+    }
+}
