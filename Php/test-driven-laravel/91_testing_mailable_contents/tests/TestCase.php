40e7539c37a9f6a81c0823e7dc14d686e587b11c
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-05-05 22:58:47 +0200

    Testing mailable contents

---
+++ Php/test-driven-laravel/91_testing_mailable_contents/tests/TestCase.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Exceptions\Handler;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+
+abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
+{
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
+
+    protected function setUp()
+    {
+        parent::setUp();
+        Mockery::getConfiguration()->allowMockingNonExistentMethods(false);
+    }
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
+    protected function disableExceptionHandling()
+    {
+        $this->app->instance(ExceptionHandler::class, new class extends Handler {
+            public function __construct() {}
+            public function report(Exception $e) {}
+            public function render($request, Exception $e) {
+                throw $e;
+            }
+        });
+    }
+}
