143f5f6c68795776e662206a3e2c4a2a204b69c3
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-05-05 17:58:19 +0200

    Storing charge details with orders

---
+++ Php/test-driven-laravel/81_storing_charge_details_with_orders/app/Order.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App;
+
+use App\Facades\OrderConfirmationNumber;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $guarded = [];
+
+    public static function forTickets($tickets, $email, $charge)
+    {
+        $order = self::create([
+            'confirmation_number' => OrderConfirmationNumber::generate(),
+            'email' => $email,
+            'amount' => $charge->amount(),
+            'card_last_four' => $charge->cardLastFour(),
+        ]);
+
+        foreach ($tickets as $ticket) {
+            $order->tickets()->save($ticket);
+        }
+
+        return $order;
+    }
+
+    public static function findByConfirmationNumber($confirmationNumber)
+    {
+        return self::where('confirmation_number', $confirmationNumber)->firstOrFail();
+    }
+
+    public function concert()
+    {
+        return $this->belongsTo(Concert::class);
+    }
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
+    public function ticketQuantity()
+    {
+        return $this->tickets()->count();
+    }
+
+    public function toArray()
+    {
+        return [
+            'confirmation_number' => $this->confirmation_number,
+            'email' => $this->email,
+            'ticket_quantity' => $this->ticketQuantity(),
+            'amount' => $this->amount,
+        ];
+    }
+}
