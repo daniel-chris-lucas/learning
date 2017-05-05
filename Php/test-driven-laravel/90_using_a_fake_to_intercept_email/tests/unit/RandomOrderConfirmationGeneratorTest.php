0b20df326b98574a8434bfd3ee277c74f1cd6f5e
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-05-05 22:43:46 +0200

    Using a fake to intercept email

---
+++ Php/test-driven-laravel/90_using_a_fake_to_intercept_email/tests/unit/RandomOrderConfirmationGeneratorTest.php
@@ -0,0 +1,62 @@
+<?php
+
+use App\RandomOrderConfirmationNumberGenerator;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class RandomOrderConfirmationNumberGeneratorTest extends TestCase
+{
+    // Must be 24 characters long
+    // Can only contain uppercase letters and numbers
+    // Cannot contain ambiguous characters
+    // All order confirmation numbers must be unique
+    // 
+    // ABCDEFGHJKLMNPQRSTUVWXYZ
+    // 23456789
+    
+    /** @test */
+    function must_be_24_characters_long()
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator;
+
+        $confirmationNumber = $generator->generate();
+
+        $this->assertEquals(24, strlen($confirmationNumber));
+    }
+
+    /** @test */
+    function can_only_contain_uppercase_letters_and_numbers()
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator;
+
+        $confirmationNumber = $generator->generate();
+
+        $this->assertRegExp('/^[A-Z0-9]+$/', $confirmationNumber);
+    }
+
+    /** @test */
+    function cannot_contain_ambiguous_characters()
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator;
+
+        $confirmationNumber = $generator->generate();
+
+        $this->assertFalse(strpos($confirmationNumber, '1'));
+        $this->assertFalse(strpos($confirmationNumber, 'I'));
+        $this->assertFalse(strpos($confirmationNumber, '0'));
+        $this->assertFalse(strpos($confirmationNumber, 'O'));
+    }
+
+    /** @test */
+    function confirmation_number_must_be_unique()
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator;
+
+        $confirmationNumbers = array_map(function ($i) use ($generator) {
+            return $generator->generate();
+        }, range(1, 100));
+
+        $this->assertCount(100, array_unique($confirmationNumbers));
+    }
+}
