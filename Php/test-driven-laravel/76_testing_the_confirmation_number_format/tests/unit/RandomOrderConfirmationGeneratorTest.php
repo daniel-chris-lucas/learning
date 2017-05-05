20da55df891f62da3c274db70db0053b0dbaa697
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-05-05 16:34:08 +0200

    Testing the confirmation number format

---
+++ Php/test-driven-laravel/76_testing_the_confirmation_number_format/tests/unit/RandomOrderConfirmationGeneratorTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\RandomOrderConfirmationNumberGenerator;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class RandomOrderConfirmationNumberGeneratorTest extends TestCase
+{
+    // Must be 24 characters long
+    // Can only contain uppercase letters and numbers
+    // Cannot contain ambiguous characters
+    // All order confirmation numbers must be unique
+    // 
+    // ABCDEFGHJKLMNPQRSTUVWXYZ
+    // 23456789
+    
+    /** @test */
+    function must_be_24_characters_long()
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator;
+
+        $confirmationNumber = $generator->generate();
+
+        $this->assertEquals(24, strlen($confirmationNumber));
+    }
+
+    /** @test */
+    function can_only_contain_uppercase_letters_and_numbers()
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator;
+
+        $confirmationNumber = $generator->generate();
+
+        $this->assertRegExp('/^[A-Z0-9]+$/', $confirmationNumber);
+    }
+
+    /** @test */
+    function cannot_contain_ambiguous_characters()
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator;
+
+        $confirmationNumber = $generator->generate();
+
+        $this->assertFalse(strpos($confirmationNumber, '1'));
+        $this->assertFalse(strpos($confirmationNumber, 'I'));
+        $this->assertFalse(strpos($confirmationNumber, '0'));
+        $this->assertFalse(strpos($confirmationNumber, 'O'));
+    }
+}
