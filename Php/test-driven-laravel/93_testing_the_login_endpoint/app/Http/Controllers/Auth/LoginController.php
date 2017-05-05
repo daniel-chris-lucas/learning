4b59b9e44b7ae5fc8be95737e7a8a60fa1c586a3
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-05-05 23:34:58 +0200

    Testing the login endpoint

---
+++ Php/test-driven-laravel/93_testing_the_login_endpoint/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login()
+    {
+        if (! Auth::attempt(request(['email', 'password']))) {
+            return redirect('/login')->withErrors([
+                'email' => ['These credentials do not match our records.'],
+            ]);
+        }
+        
+        return redirect('/backstage/concerts');
+    }
+}
