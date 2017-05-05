a7904a0dd951916f2a0a57c000a9ce02dc24f668
Daniel Lucas <daniel.chris.lucas@gmail.com>
2017-05-05 15:53:42 +0200

    Fixing the test suite

---
+++ Php/test-driven-laravel/72_fixing_the_test_suite/tests/unit/Billing/FakePaymentGatewayTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Billing\FakePaymentGateway;
+use App\Billing\PaymentFailedException;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class FakePaymentGatewayTest extends TestCase
+{
+    use PaymentGatewayContractTests;
+
+    protected function getPaymentGateway()
+    {
+        return new FakePaymentGateway;
+    }
+
+    /** @test */
+    function running_a_hook_before_the_first_charge()
+    {
+        $paymentGateway = new FakePaymentGateway;
+        $timesCallbackRan = 0;
+
+        $paymentGateway->beforeFirstCharge(function ($paymentGateway) use (&$timesCallbackRan) {
+            $timesCallbackRan++;
+            $paymentGateway->charge(2500, $paymentGateway->getValidTestToken());
+            $this->assertEquals(2500, $paymentGateway->totalCharges());
+        });
+
+        $paymentGateway->charge(2500, $paymentGateway->getValidTestToken());
+        $this->assertEquals(1, $timesCallbackRan);
+        $this->assertEquals(5000, $paymentGateway->totalCharges());
+    }
+}
