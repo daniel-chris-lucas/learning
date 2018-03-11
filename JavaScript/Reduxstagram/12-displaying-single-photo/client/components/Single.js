0b552844b3ed283e75cf2739d502570e57fc8921
Daniel Lucas <daniel.chris.lucas@gmail.com>
2018-03-11 18:19:35 +0100

    Displaying the Single Photo Component

---
+++ JavaScript/Reduxstagram/12-displaying-single-photo/client/components/Single.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
+// import comments
+
+const Single = React.createClass({
+    render() {
+        // index of the post
+        const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+        const post = this.props.posts[i];
+        
+        // get us the post
+        return (
+            <div className="single-photo">
+                <Photo i={i} post={post} {...this.props} />
+                <Comments></Comments>
+            </div>
+        )
+    }
+});
+
+export default Single;
