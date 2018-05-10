69685e5811f17bfcc4c0a0d1012aea511de6230a
Daniel Lucas <daniel.chris.lucas@gmail.com>
2018-05-10 22:16:22 +0200

    Added form validation

--- Php/symfony/autotrader/src/CarBundle/Controller/DefaultController.php
+++ Php/symfony/autotrader/src/CarBundle/Controller/DefaultController.php
@@ -5,20 +5,38 @@ namespace CarBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/our-cars", name="offer")
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $carRepository = $this->getDoctrine()->getRepository('CarBundle:Car');
         $cars = $carRepository->findCarsWithDetails();
+
         $form = $this->createFormBuilder()
             ->setMethod('GET')
-            ->add('search', TextType::class)
+            ->add('search', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 2])
+                ]
+            ])
             ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            die('form submitted');
+        }
 
         return $this->render('CarBundle:Default:index.html.twig', [
             'cars' => $cars,
