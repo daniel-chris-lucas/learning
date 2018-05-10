9f969048308d37382d40ddc86277cdb0e116e9f1
Daniel Lucas <daniel.chris.lucas@gmail.com>
2018-05-10 22:20:01 +0200

    Added form for make entity

---
+++ Php/symfony/autotrader/src/CarBundle/Form/MakeType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace CarBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MakeType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CarBundle\Entity\Make'
+        ));
+    }
+}
