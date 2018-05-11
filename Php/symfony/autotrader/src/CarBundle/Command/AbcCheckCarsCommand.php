63fe90a10c6457addc0cdebe99067e2f895b49b6
Daniel Lucas <daniel.chris.lucas@gmail.com>
2018-05-11 02:03:02 +0200

    Added progress bar with format aguments

--- Php/symfony/autotrader/src/CarBundle/Command/AbcCheckCarsCommand.php
+++ Php/symfony/autotrader/src/CarBundle/Command/AbcCheckCarsCommand.php
@@ -3,6 +3,7 @@
 namespace CarBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -15,7 +16,7 @@ class AbcCheckCarsCommand extends ContainerAwareCommand
         $this
             ->setName('abc:check-cars')
             ->setDescription('...')
-            ->addArgument('argument', InputArgument::OPTIONAL, 'Argument description')
+            ->addArgument('format', InputArgument::OPTIONAL, 'Progress format')
             ->addOption('option', null, InputOption::VALUE_NONE, 'Option description')
         ;
     }
@@ -23,12 +24,22 @@ class AbcCheckCarsCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $manager = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $dataChecker = $this->getContainer()->get('car.data_checker');
         $carRepository = $manager->getRepository('CarBundle:Car');
         $cars = $carRepository->findAll();
+        $bar = new ProgressBar($output, count($cars));
+
+        $argument = $input->getArgument('format');
+
+        $bar->setFormat($argument);
+        $bar->start();
 
         foreach($cars as $car) {
-            $output->writeln($car->getId());
+            $dataChecker->checkCar($car);
+            $bar->advance();
         }
+
+        $bar->finish();
     }
 
 }
